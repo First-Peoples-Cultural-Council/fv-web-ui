ac09cabef963cc1773c8cc35a2e6e378c400fbc6
Chad Oakenfold <web@oakenfold.ca>
2019-07-22 15:50:04 -0700

    AlphabetListView: Updated cy.logs

--- frontend/app/assets/javascripts/views/components/AlphabetListView/__cypress__/AlphabetListView.js
+++ frontend/app/assets/javascripts/views/components/AlphabetListView/__cypress__/AlphabetListView.js
@@ -18,6 +18,7 @@ describe('AlphabetListView', () => {
     cy.visit('http://0.0.0.0:3001/nuxeo/app/explore/FV/sections/Data/Haisla/Haisla/Haisla/learn/words')
     const unselectedColor = 'rgb(60, 52, 52)'
     // No message, button, or selected letters
+    cy.log('No message, button, or selected letters')
     cy.queryByText(/showing words that start with the letter/i).should('not.exist')
     cy.queryByText(/stop browsing alphabetically/i).should('not.exist')
     cy.getByTestId('AlphabetListView').within(() => {
@@ -29,10 +30,12 @@ describe('AlphabetListView', () => {
     })
 
     // Now select a letter
+    cy.log('Now select a letter')
     const letter = 'x̄°'
     cy.getByText(letter).click()
 
     // Is it highlighted?
+    cy.log('Is it highlighted?')
     cy.getByTestId('AlphabetListView').within(() => {
       cy.getByText(letter)
         .should('have.css', 'color')
@@ -40,13 +43,16 @@ describe('AlphabetListView', () => {
     })
 
     // Is the message and clear button displayed?
+    cy.log('Is the message and clear button displayed?')
     cy.queryByText(/showing words that start with the letter/i).should('exist')
     cy.queryByText(/stop browsing alphabetically/i).should('exist')
 
     // Reset
+    cy.log('Reset')
     cy.queryByText(/stop browsing alphabetically/i).click()
 
     // Ensure all is back to normal...
+    cy.log('Ensure all is back to normal...')
     cy.queryByText(/showing words that start with the letter/i).should('not.exist')
     cy.queryByText(/stop browsing alphabetically/i).should('not.exist')
     cy.getByTestId('AlphabetListView').within(() => {
@@ -60,9 +66,13 @@ describe('AlphabetListView', () => {
 
   it('Direct link: displays message, selected letter, & stop browsing buton', () => {
     cy.log('NOTE: Test expects to be run with `npm run startPreprod`')
+
+    cy.log('Direct visit a url with a letter selected')
     cy.visit(
       'http://0.0.0.0:3001/nuxeo/app/explore/FV/sections/Data/Haisla/Haisla/Haisla/learn/words/alphabet/k%CC%93%C2%B0'
     )
+    // Message & "Stop Browsing" button displayed; a letter is selected
+    cy.log('Ensure message & "Stop Browsing" button is displayed and a letter is selected')
     cy.getByText(/showing words that start with the letter/i).should('exist')
     cy.getByTestId('AlphabetListView').within(() => {
       cy.getByText('k̓°')
