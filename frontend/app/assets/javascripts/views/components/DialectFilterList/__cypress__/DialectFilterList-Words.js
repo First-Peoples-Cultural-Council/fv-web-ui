8ac6603f335ee8e223c2fd26b53e11c390b8fc4e
Nolan Van Hell <nolan@fpcc.ca>
2019-10-28 12:32:40 -0700

    FW-528: Update Cypress to 3.5.0

--- frontend/app/assets/javascripts/views/components/DialectFilterList/__cypress__/DialectFilterList-Words.js
+++ frontend/app/assets/javascripts/views/components/DialectFilterList/__cypress__/DialectFilterList-Words.js
@@ -18,6 +18,12 @@ describe('DialectFilterList-Words.js > DialectFilterList', () => {
     })
   })
   it('FW-255: ‘Create’ button is not working after filtering by Category (Navigate to a category, click Create Word)', () => {
+    /*
+      Temporary line to force the test to fail until it is updated.
+     */
+    cy.log('Forcing the test to fail until it is updated for dev.')
+    cy.expect(true).to.equal(false)
+
     cy.log('NOTE: Test expects to be run with `npm run startPreprod`')
     cy.login()
     cy.visit('/explore/FV/Workspaces/Data/Haisla/Haisla/Haisla/learn/words')
