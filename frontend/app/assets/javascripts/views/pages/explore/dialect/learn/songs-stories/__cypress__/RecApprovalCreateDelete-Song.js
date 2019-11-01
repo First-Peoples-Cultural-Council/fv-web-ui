4d3e80c0fa2eb1929b78b037a488097ab7f2a662
Nolan Van Hell <nolan@fpcc.ca>
2019-11-01 09:30:54 -0700

    FW-528: Update RecApprovalCreateDelete-Song.js

--- frontend/app/assets/javascripts/views/pages/explore/dialect/learn/songs-stories/__cypress__/RecApprovalCreateDelete-Song.js
+++ frontend/app/assets/javascripts/views/pages/explore/dialect/learn/songs-stories/__cypress__/RecApprovalCreateDelete-Song.js
@@ -12,6 +12,7 @@ describe('RecApprovalCreateDelete-Song.js > RecApprovalCreateDelete-Song', () =>
       url: 'https://dev.firstvoices.com/nuxeo/startup',
     })
     cy.visit('/explore/FV/Workspaces/Data/TEst/Test/TestLanguageThree/learn/songs')
+    cy.wait(500)
     cy.queryByText('TestSongTitle').should('not.exist')
     cy.queryByText('Continue to song').should('not.exist')
 
@@ -204,7 +205,7 @@ describe('RecApprovalCreateDelete-Song.js > RecApprovalCreateDelete-Song', () =>
     cy.getByText('Delete book success').should('exist')
 
     cy.getByText('Return To Previous Page').click()
-
+    cy.wait(500)
     cy.queryByText('TestSongTitle').should('not.exist')
     cy.queryByText('Continue to song').should('not.exist')
   })
