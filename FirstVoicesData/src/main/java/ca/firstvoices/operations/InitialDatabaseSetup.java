f2ea135c3b277054196caa13bdb7e0d7e700b146
Nolan Van Hell <nolan@fpcc.ca>
2019-11-19 17:50:20 -0800

    FW-612: Add duplicate checks to InitialDatabaseSetup.java
    Additionally removed the postContentCreationHandler extension point, added comments,
    and cleaned up the code.

--- FirstVoicesData/src/main/java/ca/firstvoices/operations/InitialDatabaseSetup.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/operations/InitialDatabaseSetup.java
@@ -1,6 +1,7 @@
 package ca.firstvoices.operations;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.nuxeo.ecm.automation.OperationException;
@@ -25,7 +26,11 @@ import static org.nuxeo.ecm.platform.usermanager.UserConfig.USERNAME_COLUMN;
 /**
  *
  */
-@Operation(id=InitialDatabaseSetup.ID, category= Constants.CAT_DOCUMENT, label="FVInitialDatabaseSetup", description="Describe here what your operation does.")
+@Operation(id=InitialDatabaseSetup.ID, category= Constants.CAT_DOCUMENT, label="FVInitialDatabaseSetup",
+        description="This operation is used to setup the inital FirstVoices backend for development.  " +
+                "It can be run multiple times without issues. Please ensure you have your environment " +
+                "variables set for CYPRESS_FV_USERNAME and CYPRESS_FV_PASSWORD as these will create an admin " +
+                "account for you.")
 public class InitialDatabaseSetup {
     
     public static final String ID = "Document.InitialDatabaseSetup";
@@ -53,54 +58,102 @@ public class InitialDatabaseSetup {
             /*
                 Create the proper folder structure.
              */
-            DocumentModel SiteWorkspace = session.createDocumentModel("/FV/Workspaces", "Site", "Workspace");
-            SiteWorkspace.setPropertyValue("dc:title", "Site");
-            SiteWorkspace = session.createDocument(SiteWorkspace);
+            boolean thisExists;
+            thisExists = session.exists(new PathRef("/FV/Workspaces/Site"));
+            if (!thisExists) {
+                DocumentModel SiteWorkspace = session.createDocumentModel("/FV/Workspaces", "Site", "Workspace");
+                SiteWorkspace.setPropertyValue("dc:title", "Site");
+                SiteWorkspace = session.createDocument(SiteWorkspace);
+            }
             
-            DocumentModel SiteSection = session.createDocumentModel("/FV/sections", "Site", "Section");
-            SiteSection.setPropertyValue("dc:title", "Site");
-            SiteSection = session.createDocument(SiteSection);
+            thisExists = session.exists(new PathRef("/FV/sections/Site"));
+            if (!thisExists) {
+                DocumentModel SiteSection = session.createDocumentModel("/FV/sections", "Site", "Section");
+                SiteSection.setPropertyValue("dc:title", "Site");
+                SiteSection = session.createDocument(SiteSection);
+            }
             
-            DocumentModel Resources = session.createDocumentModel("/FV/Workspaces/Site", "Resources", "FVResources");
-            Resources.setPropertyValue("dc:title", "Resources");
-            Resources = session.createDocument(Resources);
+            thisExists = session.exists(new PathRef("/FV/Workspaces/Site/Resources"));
+            if (!thisExists) {
+                DocumentModel Resources = session.createDocumentModel("/FV/Workspaces/Site", "Resources", "FVResources");
+                Resources.setPropertyValue("dc:title", "Resources");
+                Resources = session.createDocument(Resources);
+            }
             
-            DocumentModel Pages = session.createDocumentModel("/FV/Workspaces/Site/Resources", "Pages", "Folder");
-            Pages.setPropertyValue("dc:title", "Pages");
-            Pages = session.createDocument(Pages);
+            thisExists = session.exists(new PathRef("/FV/Workspaces/Site/Resources/Pages"));
+            if (!thisExists) {
+                DocumentModel Pages = session.createDocumentModel("/FV/Workspaces/Site/Resources", "Pages", "Folder");
+                Pages.setPropertyValue("dc:title", "Pages");
+                Pages = session.createDocument(Pages);
+            }
+    
+            thisExists = session.exists(new PathRef("/FV/Workspaces/SharedData/Shared Categories"));
+            if (!thisExists) {
+                DocumentModel SharedCategories = session.createDocumentModel("/FV/Workspaces/SharedData", "Shared Categories", "FVCategories");
+                SharedCategories.setPropertyValue("dc:title", "Shared Categories");
+                SharedCategories = session.createDocument(SharedCategories);
+            }
+    
+            thisExists = session.exists(new PathRef("/FV/Workspaces/SharedData/Shared Links"));
+            if (!thisExists) {
+                DocumentModel SharedLinks = session.createDocumentModel("/FV/Workspaces/SharedData", "Shared Links", "FVLinks");
+                SharedLinks.setPropertyValue("dc:title", "Shared Links");
+                SharedLinks = session.createDocument(SharedLinks);
+            }
+    
+            thisExists = session.exists(new PathRef("/FV/Workspaces/SharedData/Shared Resources"));
+            if (!thisExists) {
+                DocumentModel SharedResources = session.createDocumentModel("/FV/Workspaces/SharedData", "Shared Resources", "FVResources");
+                SharedResources.setPropertyValue("dc:title", "Shared Resources");
+                SharedResources = session.createDocument(SharedResources);
+            }
             
-            DocumentModel SharedCategories = session.createDocumentModel("/FV/Workspaces/SharedData", "Shared Categories", "FVCategories");
-            SharedCategories.setPropertyValue("dc:title", "Shared Categories");
-            SharedCategories = session.createDocument(SharedCategories);
-            
-            DocumentModel SharedLinks = session.createDocumentModel("/FV/Workspaces/SharedData", "Shared Links", "FVLinks");
-            SharedLinks.setPropertyValue("dc:title", "Shared Links");
-            SharedLinks = session.createDocument(SharedLinks);
-            
-            DocumentModel SharedResources = session.createDocumentModel("/FV/Workspaces/SharedData", "Shared Resources", "FVResources");
-            SharedResources.setPropertyValue("dc:title", "Shared Resources");
-            SharedResources = session.createDocument(SharedResources);
 
             /*
                 Create the user groups.
              */
-            DocumentModel LanguageAdministrators = userManager.getBareGroupModel();
-            LanguageAdministrators.setProperty("group", "groupname", "language_administrators");
-            LanguageAdministrators.setProperty("group", "grouplabel", "Language Administators");
-            userManager.createGroup(LanguageAdministrators);
+            thisExists = userManager.getGroup("language_administrators") != null;
+            if (!thisExists) {
+                DocumentModel LanguageAdministrators = userManager.getBareGroupModel();
+                LanguageAdministrators.setProperty("group", "groupname", "language_administrators");
+                LanguageAdministrators.setProperty("group", "grouplabel", "Language Administators");
+                userManager.createGroup(LanguageAdministrators);
+            }
+    
+            thisExists = userManager.getGroup("recorders") != null;
+            if (!thisExists) {
+                DocumentModel Recorders = userManager.getBareGroupModel();
+                Recorders.setProperty("group", "groupname", "recorders");
+                Recorders.setProperty("group", "grouplabel", "Recorders");
+                userManager.createGroup(Recorders);
+            }
+    
+            thisExists = userManager.getGroup("recorders_with_approval") != null;
+            if (!thisExists) {
+                DocumentModel RecordersWithApproval = userManager.getBareGroupModel();
+                RecordersWithApproval.setProperty("group", "groupname", "recorders_with_approval");
+                RecordersWithApproval.setProperty("group", "grouplabel", "Recorders With Approval");
+                userManager.createGroup(RecordersWithApproval);
+            }
             
-            DocumentModel Recorders = userManager.getBareGroupModel();
-            Recorders.setProperty("group", "groupname", "recorders");
-            Recorders.setProperty("group", "grouplabel", "Recorders");
-            userManager.createGroup(Recorders);
-            
-            DocumentModel RecordersWithApproval = userManager.getBareGroupModel();
-            RecordersWithApproval.setProperty("group", "groupname", "recorders_with_approval");
-            RecordersWithApproval.setProperty("group", "grouplabel", "Recorders With Approval");
-            userManager.createGroup(RecordersWithApproval);
-            
+            /*
+                Add new user groups as subgroups of group "members" and keep any existing subgroups.
+             */
             DocumentModel members = userManager.getGroupModel("members");
-            members.setProperty("group", "subGroups", Arrays.asList("language_administrators", "recorders", "recorders_with_approval"));
+            Object existingSubGroups = members.getProperty("group", "subGroups");
+            String existingSubGroupsString = existingSubGroups.toString();
+            existingSubGroupsString = existingSubGroupsString.substring(1, existingSubGroupsString.length()-1);
+            List<String> newSubGroups;
+            if (existingSubGroupsString.length() == 0) {
+                newSubGroups = new ArrayList<>();
+            } else {
+                newSubGroups = new ArrayList<>(Arrays.asList(existingSubGroupsString.split(", ")));
+            }
+            newSubGroups.add("language_administrators");
+            newSubGroups.add("recorders");
+            newSubGroups.add("recorders_with_approval");
+            List<String> noDuplicates = newSubGroups.stream().distinct().collect(Collectors.toList());
+            members.setProperty("group", "subGroups", noDuplicates);
             userManager.updateGroup(members);
             
             /*
@@ -143,13 +196,16 @@ public class InitialDatabaseSetup {
             /*
                 Create admin user.
              */
-            String[] groups ={"administrators"};
-            DocumentModel userDoc = userManager.getBareUserModel();
-            userDoc.setProperty(SCHEMA_NAME, USERNAME_COLUMN, username);
-            userDoc.setProperty(SCHEMA_NAME, PASSWORD_COLUMN, password);
-            userDoc.setPropertyValue(GROUPS_COLUMN, groups);
-            userDoc.setProperty(SCHEMA_NAME, EMAIL_COLUMN, "@.");
-            userDoc = userManager.createUser(userDoc);
+            thisExists = userManager.getUserModel(username) != null;
+            if (!thisExists) {
+                String[] groups ={"administrators"};
+                DocumentModel userDoc = userManager.getBareUserModel();
+                userDoc.setProperty(SCHEMA_NAME, USERNAME_COLUMN, username);
+                userDoc.setProperty(SCHEMA_NAME, PASSWORD_COLUMN, password);
+                userDoc.setPropertyValue(GROUPS_COLUMN, groups);
+                userDoc.setProperty(SCHEMA_NAME, EMAIL_COLUMN, "@.");
+                userDoc = userManager.createUser(userDoc);
+            }
             
             return session.getRootDocument();
         } else {
@@ -157,10 +213,13 @@ public class InitialDatabaseSetup {
         }
     }
     
-    private void addSection(String sectionId, DocumentModel currentDocument) {
+    /*
+        Helper method to set a publication target for a document.
+     */
+    private void addSection(String targetSectionId, DocumentModel sourceDocument) {
         
-        if (sectionId != null && currentDocument.hasSchema(SCHEMA_PUBLISHING)) {
-            String[] sectionIdsArray = (String[]) currentDocument.getPropertyValue(SECTIONS_PROPERTY_NAME);
+        if (targetSectionId != null && sourceDocument.hasSchema(SCHEMA_PUBLISHING)) {
+            String[] sectionIdsArray = (String[]) sourceDocument.getPropertyValue(SECTIONS_PROPERTY_NAME);
             
             List<String> sectionIdsList = new ArrayList<String>();
             
@@ -170,12 +229,12 @@ public class InitialDatabaseSetup {
                 sectionIdsList = new ArrayList<String>(sectionIdsList);
             }
             
-            sectionIdsList.add(sectionId);
+            sectionIdsList.add(targetSectionId);
             String[] sectionIdsListIn = new String[sectionIdsList.size()];
             sectionIdsList.toArray(sectionIdsListIn);
             
-            currentDocument.setPropertyValue(SECTIONS_PROPERTY_NAME, sectionIdsListIn);
-            session.saveDocument(currentDocument);
+            sourceDocument.setPropertyValue(SECTIONS_PROPERTY_NAME, sectionIdsListIn);
+            session.saveDocument(sourceDocument);
             session.save();
         }
     }
