a4e934acfcf98466d704bc178cd45b55c344dab3
Nolan Van Hell <nolan@fpcc.ca>
2019-11-20 19:20:39 -0800

    FW-528: Change Cypress commands.js to use cypress.json target env

--- frontend/scripts/TestDatabaseTeardown.sh
+++ frontend/scripts/TestDatabaseTeardown.sh
@@ -102,8 +102,8 @@ if [[ "$1" == "http://127.0.0.1:8080" ]]; then
     echo
     echo "Deleting TEst FVLanguageFamily directory"
     response=$(curl -o /dev/null -s -w "%{response_code}\n" -X POST 'http://127.0.0.1:8080/nuxeo/site/automation/Document.Delete' -H 'Nuxeo-Transaction-Timeout: 3' -H 'X-NXproperties: *' -H 'X-NXRepository: default' -H 'X-NXVoidOperation: false' -H 'content-type: application/json' -d '{"params":{},"input":"/FV/Workspaces/Data/TEst","context":{}}' -u $CYPRESS_FV_USERNAME:$CYPRESS_FV_PASSWORD)
-    if [[ "response" -ne 200 ]]; then
-        echo -e 'TEst FVLanguageFamily deletion failed: Error ' response ' \n'; exit 1
+    if [[ "response" -ne 204 ]]; then
+        echo -e 'TEst FVLanguageFamily deletion failed: Error ' $response ' \n'; exit 1
         echo
     fi
 fi
