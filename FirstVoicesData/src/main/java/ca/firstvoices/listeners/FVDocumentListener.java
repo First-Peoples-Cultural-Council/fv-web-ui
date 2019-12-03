4cf1032b79e362bb9459ece7ee621fc0ffbdefc3
Nolan Van Hell <nolan@fpcc.ca>
2019-12-02 22:26:43 -0800

    FW-698: Update FVDocumentListener.java

--- FirstVoicesData/src/main/java/ca/firstvoices/listeners/FVDocumentListener.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/listeners/FVDocumentListener.java
@@ -1,6 +1,24 @@
 package ca.firstvoices.listeners;
 
+import org.nuxeo.ecm.core.api.model.Property;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventContext;
+import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+
+import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.event.Event;
@@ -9,6 +27,9 @@ import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.schema.DocumentType;
+import org.nuxeo.ecm.automation.core.operations.document.GetDocumentParent;
+import org.nuxeo.ecm.core.schema.types.SimpleType;
+import org.nuxeo.ecm.core.schema.types.Type;
 
 import java.util.Arrays;
 
@@ -16,6 +37,9 @@ public class FVDocumentListener implements EventListener {
   
   @Context
   protected CoreSession session;
+  
+  @Context
+  protected CoreSession sessionTwo;
   
   @Override
   public void handleEvent(Event event) {
@@ -28,33 +52,65 @@ public class FVDocumentListener implements EventListener {
     if (!(ctx instanceof DocumentEventContext))
       return;
     
+    CoreSession session = ctx.getCoreSession();
+    
     DocumentEventContext docCtx = (DocumentEventContext) ctx;
     
-    DocumentModel document = docCtx.getSourceDocument();
+    DocumentModel document = ((DocumentEventContext) ctx).getSourceDocument();
+    if (document == null) {
+      return;
+    }
+    DocumentRef testRef = session.getParentDocumentRef(document.getRef());
     
     if (!(checkType(document)))
       return;
     
-    dialect = getParent(document.getRef(), "FVDialect");
-    language = getParent(document.getRef(), "FVLanguage");
-    languageFamily = getParent(document.getRef(), "FVLanguageFamily");
-    
-    if (languageFamily != null && !languageFamily.toString().trim().isEmpty()) {
-      document.setPropertyValue("fva:family", languageFamily.getId());
+  
+  
+    DocumentModel parent = session.getParentDocument(document.getRef());
+    String currentType = "FVDialect";
+    while (parent != null && !currentType.equals(parent.getType())) {
+      parent = session.getParentDocument(parent.getRef());
     }
-    
-    if (language != null && !language.toString().trim().isEmpty()) {
-      document.setPropertyValue("fva:language", language.getId());
+    dialect = parent;
+  
+    parent = session.getParentDocument(document.getRef());
+    currentType = "FVDialect";
+    while (parent != null && !currentType.equals(parent.getType())) {
+      parent = session.getParentDocument(parent.getRef());
     }
+    language = parent;
+  
+    parent = session.getParentDocument(document.getRef());
+    currentType = "FVDialect";
+    while (parent != null && !currentType.equals(parent.getType())) {
+      parent = session.getParentDocument(parent.getRef());
+    }
+    languageFamily = parent;
     
-    if (dialect != null && !dialect.toString().trim().isEmpty()) {
-      document.setPropertyValue("fva:dialect", dialect.getId());
+    if (languageFamily != null) {
+//      document.setPropertyValue("fva:family", languageFamily.getId());
+    }
+
+    if (language != null) {
+//      document.setPropertyValue("fva:language", language.getId());
+    }
+
+    if (dialect != null) {
+//      document.setPropertyValue("fva:dialect", dialect.getId());
     }
     
   }
   
   private DocumentModel getParent(DocumentRef inputDoc, String currentType) {
+    
+    if (currentType == null) {
+      return session.getParentDocument(inputDoc);
+    }
     currentType = currentType.trim();
+    if (currentType.length() == 0) {
+      return session.getParentDocument(inputDoc);
+    }
     DocumentModel parent = session.getParentDocument(inputDoc);
     while (parent != null && !currentType.equals(parent.getType())) {
       parent = session.getParentDocument(parent.getRef());
@@ -90,11 +146,8 @@ public class FVDocumentListener implements EventListener {
         "FVVideo",
         "FVWord",
     };
-    
-    if ( Arrays.stream(types).parallel().anyMatch(currentType.toString()::contains) ) {
-      return true;
-    }
-    return false;
+  
+    return Arrays.stream(types).parallel().anyMatch(currentType.toString()::contains);
   }
   
 }
