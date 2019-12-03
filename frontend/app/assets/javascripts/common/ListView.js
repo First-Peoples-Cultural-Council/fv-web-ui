5143a24cade503ad494cf1493900424b1c6343fc
Chad Oakenfold <web@oakenfold.ca>
2019-12-03 10:47:30 -0800

    Sort button active styling on Workspaces

--- frontend/app/assets/javascripts/common/ListView.js
+++ frontend/app/assets/javascripts/common/ListView.js
@@ -8,6 +8,10 @@ import NavigationHelpers, {
 import Checkbox from 'views/components/Form/Common/Checkbox'
 import ConfirmationDelete from 'views/components/Confirmation'
 
+import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp'
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
+import UnfoldMore from '@material-ui/icons/UnfoldMore'
+
 // batchTitle
 // ============================================
 export const batchTitle = ({ uidsNotDeleted = [], selected, setSelected, copyDeselect, copySelect }) => {
@@ -118,29 +122,29 @@ export const deleteSelected = ({ batchConfirmationAction, deletedUids, selected,
 
 // getIcon
 // ============================================
-export const getIcon = ({ field, sortOrder, sortBy }) => {
+export const getIcon = ({ field, sortOrder, sortBy, color = 'inherit' }) => {
   if (sortBy === field) {
     return sortOrder === 'asc' ? (
       // SORTED ASCENDING
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
-        <path d="M0 0h24v24H0z" fill="none" />
-      </svg>
+      <KeyboardArrowUp color={color} />
     ) : (
       // SORTED DESCENDING
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-        <path d="M0 0h24v24H0z" fill="none" />
-      </svg>
+      <KeyboardArrowDown color={color} />
     )
   }
   // UNSORTED
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-      <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z" />
-    </svg>
-  )
+  return <UnfoldMore color={color} />
+}
+
+// getSortState
+// If sorted returns sortOrder (ie: 'asc' or 'desc')
+// If not sorted returns undefined
+// ============================================
+export const getSortState = ({ field, sortOrder, sortBy }) => {
+  if (sortBy === field) {
+    return sortOrder
+  }
+  return undefined
 }
 
 // getUidsThatAreNotDeleted
