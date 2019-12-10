d61ff2d783273beb18b50734b968b8c5b343e501
Nolan Van Hell <nolan@fpcc.ca>
2019-12-10 14:01:27 -0800

    FW-698: Refactored listener to use a service for ancestor assignment

--- FirstVoicesData/src/main/java/ca/firstvoices/listeners/FVDocumentListener.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/listeners/FVDocumentListener.java
@@ -1,5 +1,6 @@
 package ca.firstvoices.listeners;
 
+import ca.firstvoices.services.AssignAncestorsService;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
@@ -7,8 +8,8 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.schema.DocumentType;
+import org.nuxeo.runtime.api.Framework;
 
 import java.util.Arrays;
 
@@ -17,14 +18,10 @@ public class FVDocumentListener implements EventListener {
   @Context
   protected CoreSession session;
   
-  @Context
-  protected CoreSession sessionTwo;
+  protected AssignAncestorsService service = Framework.getService(AssignAncestorsService.class);
   
   @Override
   public void handleEvent(Event event) {
-    DocumentModel dialect;
-    DocumentModel language;
-    DocumentModel languageFamily;
     
     // Get event context and return if not DocumentEventContext
     EventContext ctx;
@@ -32,81 +29,21 @@ public class FVDocumentListener implements EventListener {
     if (!(ctx instanceof DocumentEventContext))
       return;
     
-    CoreSession session = ctx.getCoreSession();
-    
-    DocumentEventContext docCtx = (DocumentEventContext) ctx;
-    
     // Get document from context and return if it is not mutable
     DocumentModel document = ((DocumentEventContext) ctx).getSourceDocument();
     if (document == null || document.isImmutable()) {
       return;
     }
-    DocumentRef testRef = session.getParentDocumentRef(document.getRef());
     
     // Check that the document is a specific type using the helper method
     if (!(checkType(document)))
       return;
     
-  
-    // Get the parent FVDialect document if it exists
-    DocumentModel parent = session.getParentDocument(document.getRef());
-    String currentType = "FVDialect";
-    while (parent != null && !currentType.equals(parent.getType())) {
-      parent = session.getParentDocument(parent.getRef());
-    }
-    dialect = parent;
-  
-    // Get the parent FVLanguage document if it exists
-    parent = session.getParentDocument(document.getRef());
-    currentType = "FVLanguage";
-    while (parent != null && !currentType.equals(parent.getType())) {
-      parent = session.getParentDocument(parent.getRef());
-    }
-    language = parent;
-  
-    // Get the parent FVLanguageFamily document if it exists
-    parent = session.getParentDocument(document.getRef());
-    currentType = "FVLanguageFamily";
-    while (parent != null && !currentType.equals(parent.getType())) {
-      parent = session.getParentDocument(parent.getRef());
-    }
-    languageFamily = parent;
+    CoreSession session = ctx.getCoreSession();
     
-    // Set the property fva:family of the new document to be the UUID of the parent FVLanguageFamily document
-    if (languageFamily != null) {
-      document.setPropertyValue("fva:family", languageFamily.getId());
-      document = session.saveDocument(document);
-    }
-  
-    // Set the property fva:language of the new document to be the UUID of the parent FVLanguage document
-    if (language != null) {
-      document.setPropertyValue("fva:language", language.getId());
-      document = session.saveDocument(document);
-    }
-  
-    // Set the property fva:dialect of the new document to be the UUID of the parent FVDialect document
-    if (dialect != null) {
-      document.setPropertyValue("fva:dialect", dialect.getId());
-      document = session.saveDocument(document);
-    }
+    // If the document is mutable and is the proper type then assign its ancestors.
+    service.assignAncestors(session, document);
     
-  }
-  
-  // Helper method to get the parent document
-  private DocumentModel getParent(DocumentRef inputDoc, String currentType) {
-    
-    if (currentType == null) {
-      return session.getParentDocument(inputDoc);
-    }
-    currentType = currentType.trim();
-    if (currentType.length() == 0) {
-      return session.getParentDocument(inputDoc);
-    }
-    DocumentModel parent = session.getParentDocument(inputDoc);
-    while (parent != null && !currentType.equals(parent.getType())) {
-      parent = session.getParentDocument(parent.getRef());
-    }
-    return parent;
   }
   
   // Helper method to check that the new document is one of the types below
