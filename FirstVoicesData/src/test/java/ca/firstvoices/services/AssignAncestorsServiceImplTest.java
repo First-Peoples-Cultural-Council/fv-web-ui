3737d7a089a919856e3f6939ea84e00919f085f8
Daniel Yona <daniel@fpcc.ca>
2019-12-18 14:39:08 -0800

    Fix unit tests - only include necessary components/deps

--- FirstVoicesData/src/test/java/ca/firstvoices/services/AssignAncestorsServiceImplTest.java
+++ FirstVoicesData/src/test/java/ca/firstvoices/services/AssignAncestorsServiceImplTest.java
@@ -1,44 +1,60 @@
 package ca.firstvoices.services;
 
-import javax.inject.Inject;
 import ca.firstvoices.testUtil.AssignAncestorsTestUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
-import org.nuxeo.runtime.test.runner.*;
-import static org.junit.Assert.*;
-import org.nuxeo.ecm.core.api.PathRef;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.elasticsearch.test.RepositoryElasticSearchFeature;
+import org.nuxeo.runtime.test.runner.*;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.*;
 
 @RunWith(FeaturesRunner.class)
-@Features({RuntimeFeature.class, CoreFeature.class, RepositoryElasticSearchFeature.class})
+@Features({AutomationFeature.class, PlatformFeature.class, RuntimeFeature.class, CoreFeature.class, RepositoryElasticSearchFeature.class})
 @RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
-@Deploy( {
-    "FirstVoicesData",
-    "org.nuxeo.ecm.platform",
-    "org.nuxeo.ecm.platform.commandline.executor",
-    "org.nuxeo.ecm.platform.picture.core",
-    "org.nuxeo.ecm.platform.rendition.core",
-    "org.nuxeo.ecm.platform.video.core",
-    "org.nuxeo.ecm.platform.audio.core",
-    "org.nuxeo.ecm.automation.scripting",
-    "org.nuxeo.ecm.platform.web.common",
-    "FirstVoicesData:OSGI-INF/extensions/test.templates.factories.xml",
-    "FirstVoicesData:OSGI-INF/extensions/test.operations.xml",
-    "org.nuxeo.elasticsearch.core:pageprovider-test-contrib.xml",
-    "org.nuxeo.elasticsearch.core:schemas-test-contrib.xml",
-    "org.nuxeo.elasticsearch.core:elasticsearch-test-contrib.xml",
-    "FirstVoicesData:OSGI-INF/extensions/fake-load-actions.xml",
-    "FirstVoicesData:OSGI-INF/extensions/fake-load-es-provider.xml",
-    "FirstVoicesData:OSGI-INF/extensions/fake-directory-sql-contrib.xml"
-} )
 
+@Deploy("org.nuxeo.binary.metadata")
+@Deploy("org.nuxeo.ecm.platform.url.core")
+@Deploy("org.nuxeo.ecm.platform.types.api")
+@Deploy("org.nuxeo.ecm.platform.types.core")
+@Deploy("org.nuxeo.ecm.platform.filemanager.api")
+@Deploy("org.nuxeo.ecm.platform.filemanager.core")
+@Deploy("org.nuxeo.ecm.platform.rendition.core")
+@Deploy("org.nuxeo.ecm.platform.tag")
+@Deploy("org.nuxeo.ecm.platform.commandline.executor")
+@Deploy("org.nuxeo.ecm.platform.convert")
+@Deploy("org.nuxeo.ecm.platform.preview")
+
+// Audio doctype
+@Deploy("org.nuxeo.ecm.platform.audio.core")
+
+// Video doctype
+@Deploy("org.nuxeo.ecm.platform.video.convert")
+@Deploy("org.nuxeo.ecm.platform.video.core")
+
+// Picture doctype
+@Deploy("org.nuxeo.ecm.platform.picture.core")
+@Deploy("org.nuxeo.ecm.platform.picture.api")
+@Deploy("org.nuxeo.ecm.platform.picture.convert")
+
+// ElasticSearch / Search
+@Deploy("org.nuxeo.elasticsearch.core:elasticsearch-test-contrib.xml")
+@Deploy("org.nuxeo.ecm.platform.search.core")
+@Deploy("org.nuxeo.ecm.platform.webapp.types")
+
+@Deploy("FirstVoicesData:OSGI-INF/ca.firstvoices.services.xml")
+@PartialDeploy(bundle = "FirstVoicesData", extensions = {TargetExtensions.ContentModel.class})
 public class AssignAncestorsServiceImplTest {
 
   private AssignAncestorsTestUtil testUtil;
@@ -85,8 +101,5 @@ public class AssignAncestorsServiceImplTest {
     assertEquals("Word should have ID of parent family property", languageFamily.getId(), TestWord.getPropertyValue("fva:family"));
     assertEquals("Word should have ID of parent language property", language.getId(), TestWord.getPropertyValue("fva:language"));
     assertEquals("Word should have ID of parent dialect property", dialect.getId(), TestWord.getPropertyValue("fva:dialect"));
-    
-    
   }
-
 }
