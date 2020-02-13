eabf97a559fd184320e973c79d35c9f022153d48
Daniel Yona <daniel@fpcc.ca>
2020-02-13 14:54:03 -0800

    FW-737 - Fix enricher unit test (just part of speech)

--- FirstVoicesEnrichers/src/test/java/ca/firstvoices/WordEnricherTest.java
+++ FirstVoicesEnrichers/src/test/java/ca/firstvoices/WordEnricherTest.java
@@ -1,70 +1,30 @@
 package ca.firstvoices;
 
-import ca.firstvoices.EnricherTestUtil;
 import ca.firstvoices.nuxeo.enrichers.WordEnricher;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.directory.test.DirectoryFeature;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.PathRef;
-import org.nuxeo.ecm.core.test.CoreFeature;
-import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
-import org.nuxeo.ecm.core.test.annotations.Granularity;
-import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
-import org.nuxeo.ecm.platform.test.PlatformFeature;
-import org.nuxeo.elasticsearch.test.RepositoryElasticSearchFeature;
-import org.nuxeo.runtime.test.runner.*;
-
-import org.nuxeo.ecm.core.io.marshallers.json.document.DocumentModelJsonWriter;
 import org.nuxeo.ecm.core.io.marshallers.json.AbstractJsonWriterTest;
 import org.nuxeo.ecm.core.io.marshallers.json.JsonAssert;
+import org.nuxeo.ecm.core.io.marshallers.json.document.DocumentModelJsonWriter;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContext;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContext.CtxBuilder;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
 
 import javax.inject.Inject;
 
-import static org.junit.Assert.*;
+@RepositoryConfig(init = DefaultRepositoryInit.class)
 
-@RunWith(FeaturesRunner.class)
-@Features({ AutomationFeature.class, PlatformFeature.class, RuntimeFeature.class, CoreFeature.class,
-    RepositoryElasticSearchFeature.class })
-@RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
+@Features({ CoreFeature.class, DirectoryFeature.class})
 
-@Deploy("org.nuxeo.binary.metadata")
-@Deploy("org.nuxeo.ecm.platform.url.core")
-@Deploy("org.nuxeo.ecm.platform.types.api")
-@Deploy("org.nuxeo.ecm.platform.types.core")
-@Deploy("org.nuxeo.ecm.platform.filemanager.api")
-@Deploy("org.nuxeo.ecm.platform.filemanager.core")
-@Deploy("org.nuxeo.ecm.platform.rendition.core")
-@Deploy("org.nuxeo.ecm.platform.tag")
-@Deploy("org.nuxeo.ecm.platform.commandline.executor")
-@Deploy("org.nuxeo.ecm.platform.convert")
-@Deploy("org.nuxeo.ecm.platform.preview")
-
-// Audio doctype
-@Deploy("org.nuxeo.ecm.platform.audio.core")
-
-// Video doctype
-@Deploy("org.nuxeo.ecm.platform.video.convert")
-@Deploy("org.nuxeo.ecm.platform.video.core")
-
-// Picture doctype
-@Deploy("org.nuxeo.ecm.platform.picture.core")
-@Deploy("org.nuxeo.ecm.platform.picture.api")
-@Deploy("org.nuxeo.ecm.platform.picture.convert")
-
-// ElasticSearch / Search
-@Deploy("org.nuxeo.elasticsearch.core:elasticsearch-test-contrib.xml")
-@Deploy("org.nuxeo.ecm.platform.search.core")
-@Deploy("org.nuxeo.ecm.platform.webapp.types")
-
-//@Deploy("FirstVoicesData")
 @Deploy("FirstVoicesNuxeo:OSGI-INF/extensions/ca.firstvoices.nuxeo.enrichers.xml")
-@Deploy("FirstVoicesNuxeo:OSGI-INF/extensions/ca.firstvoices.enrichers.operations.xml")
-@PartialDeploy(bundle = "FirstVoicesData", extensions = { TargetExtensions.ContentModel.class })
+@Deploy("FirstVoicesNuxeo.Test:OSGI-INF/extensions/fv-word-enricher-test-data.xml")
 public class WordEnricherTest extends AbstractJsonWriterTest.Local<DocumentModelJsonWriter, DocumentModel>{
 
   public WordEnricherTest(){
@@ -77,72 +37,31 @@ public class WordEnricherTest extends AbstractJsonWriterTest.Local<DocumentModel
   @Inject
   protected CoreSession session;
 
+  DocumentModel word = null;
+
   @Before
   public void setUpTest() {
-    testUtil = new EnricherTestUtil();
-
-    assertNotNull("Should have a valid session", session);
-    assertNotNull("Should have a valid test utilities obj", testUtil);
-
-    testUtil.createSetup(session);
+    // Create a new FVWord document
+    word = session.createDocumentModel("/", "TestWord", "FVWord");
+    word = session.createDocument(word);
   }
 
   @Test
-  public void enricherTest() throws Exception {
+  public void testPartOfSpeech() throws Exception {
 
-    // Get the DocumentModels for each of the parent documents
-    DocumentModel languageFamily = testUtil.getCurrentLanguageFamily();
-    assertNotNull("Language family cannot be null", languageFamily);
-    DocumentModel language = testUtil.getCurrentLanguage();
-    assertNotNull("Language cannot be null", language);
-    DocumentModel dialect = testUtil.getCurrentDialect();
-    assertNotNull("Dialect cannot be null", dialect);
-    DocumentModel dictionary = testUtil.getCurrentDictionary();
-    assertNotNull("Dictionary cannot be null", dictionary);
+    word.setPropertyValue("fv-word:part_of_speech", "event_activity_verb_like_word");
+    session.saveDocument(word);
 
-    // Create a new child document
-    DocumentModel TestWord = testUtil.createDocument(session,
-        session.createDocumentModel("/FV/Family/Language/Dialect/Dictionary", "TestWord", "FVWord"));
-    assertNotNull("Word cannot be null", TestWord);
-    // Commit to sesrver: session.save?
-    session.save();
-    String TestWordID = TestWord.getId();
-    System.out.println(TestWordID);
-    System.out.println(TestWord.getProperty("title"));
+    RenderingContext ctx = CtxBuilder.enrichDoc(WordEnricher.NAME).properties("fv-word").get();
+    JsonAssert json = jsonAssert(word, ctx);
 
-    DocumentModel TestPhrase = testUtil.createDocument(session,
-        session.createDocumentModel("/FV/Family/Language/Dialect/Dictionary", "TestPhrase", "FVPhrase"));
-    assertNotNull("Word cannot be null", TestPhrase);
-    // Commit to sesrver: session.save?
-    session.save();
-    String TestPhraseID = TestPhrase.getId();
-    System.out.println(TestPhraseID);
-    String arr[] = { TestPhraseID };
+    json = json.has("contextParameters").isObject();
+    json.properties(1);
 
-    TestWord.setPropertyValue("fv-word:related_phrases", arr);
-    TestWord.setPropertyValue("dc:creator", "dummy");
-    TestWord.setPropertyValue("fv-word:part_of_speech", "noun");
+    // Ensure word has been enricher (i.e. contextParamemers -> word is present)
+    json = json.has(WordEnricher.NAME).isObject();
 
-    TestPhrase.setPropertyValue("dc:creator", "dummy2");
-    System.out.println(TestWord.getProperty("fv-word:related_phrases"));
-    System.out.println(TestWord.getProperty("dc:creator"));
-    System.out.println(TestWord.getProperty("fv-word:part_of_speech"));
-    System.out.println(TestPhrase.getProperty("dc:creator"));
-    session.save();
-
-    // DocumentModel obj = session.getDocument(new PathRef("/")); WordEnricher.NAME
-    RenderingContext ctx = CtxBuilder.enrichDoc("acl").get();
-    JsonAssert json = jsonAssert(TestWord, ctx);
-    System.out.println(json.toString());
-    //json = json.has("contextParameters").isObject();
-    //json.properties(1);
-    //json.has(WordEnricher.NAME).isObject();
-
-    // EX. Create phrase X, give title & props, get ID, add to 'fv:relatedphrases',
-    // call word w/ enricher (look up how to query w/ enrichers - want word
-    // enricher)
-    // session.
-    // TestWord.setPropertyValue("uid", "c2");
-    // System.out.println(TestWord.getPropertyValue("uid"));
+    // Ensure word has been enriched (i.e. id converted to label)
+    json.has("part_of_speech").isEquals("Event/Activity (Verb-like word)");
   }
 }
