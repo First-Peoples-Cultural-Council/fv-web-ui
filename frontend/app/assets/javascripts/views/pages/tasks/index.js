bb081a84c3d38be1cf7869f0896e7b9b18f78743
Chad Oakenfold <web@oakenfold.ca>
2020-02-14 18:54:40 -0800

    FW-636 Tasks page with new list view

--- frontend/app/assets/javascripts/views/pages/tasks/index.js
+++ frontend/app/assets/javascripts/views/pages/tasks/index.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
-import React from 'react'
+import React, { Component, Suspense } from 'react'
 import PropTypes from 'prop-types'
 import Immutable from 'immutable'
 
@@ -32,16 +32,11 @@ import { fetchDialect2 } from 'providers/redux/reducers/fvDialect'
 import { fetchUserDialects } from 'providers/redux/reducers/fvUser'
 
 import selectn from 'selectn'
-
 import FVButton from 'views/components/FVButton'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
 
+import StringHelpers from 'common/StringHelpers'
 import ProviderHelpers from 'common/ProviderHelpers'
 import NavigationHelpers from 'common/NavigationHelpers'
 import DocumentView from 'views/components/Document/view'
@@ -50,11 +45,12 @@ import PromiseWrapper from 'views/components/Document/PromiseWrapper'
 import GroupAssignmentDialog from 'views/pages/users/group-assignment-dialog'
 import IntlService from 'views/services/intl'
 import '!style-loader!css-loader!./Tasks.css'
-
+import { dictionaryListSmallScreenColumnDataTemplate } from 'views/components/Browsing/DictionaryListSmallScreen'
+const DictionaryList = React.lazy(() => import('views/components/Browsing/DictionaryList'))
 const intl = IntlService.instance
 
 const { func, object } = PropTypes
-export class Tasks extends React.Component {
+export class Tasks extends Component {
   static propTypes = {
     // REDUX: reducers/state
     computeDialect2: object.isRequired,
@@ -112,70 +108,85 @@ export class Tasks extends React.Component {
   render() {
     const userID = selectn('response.id', this.props.computeLogin)
 
-    const computeEntities = Immutable.fromJS([
-      {
-        id: userID,
-        entity: this.props.computeUserTasks,
-      },
-      {
-        id: userID,
-        entity: this.props.computeUserDialects,
-      },
-      {
-        id: this.state.lastActionedTaskId,
-        entity: this.props.computeUserTasksReject,
-      },
-    ])
-
-    const computeUserDialects = ProviderHelpers.getEntry(this.props.computeUserDialects, userID)
     const _computeUserTasks = ProviderHelpers.getEntry(this.props.computeUserTasks, userID)
 
     const computeDialect = ProviderHelpers.getEntry(
       this.props.computeDialect2,
       selectn('properties.docinfo:documentId', this.state.selectedPreapprovalTask)
     )
-    // Tasks: General
-    const userTasks = this.generateUserTasks(selectn('response', _computeUserTasks))
-    const userTasksTable =
-      userTasks.length > 0 ? (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <span className="Tasks__colHeader">{intl.trans('document_title', 'Document Title', 'words')}</span>
-              </TableCell>
-              <TableCell>
-                <span className="Tasks__colHeader">{intl.trans('task_type', 'Task Type', 'words')}</span>
-              </TableCell>
-              <TableCell>
-                <span className="Tasks__colHeader Tasks__colHeader--actions">
-                  {intl.trans('actions', 'Actions', 'words')}
-                </span>
-              </TableCell>
-              <TableCell>
-                <span className="Tasks__colHeader">{intl.trans('task_due_date', 'Task Due Date', 'words')}</span>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>{userTasks}</TableBody>
-        </Table>
-      ) : null
 
-    // Tasks: Registration
+    const computeUserDialects = ProviderHelpers.getEntry(this.props.computeUserDialects, userID)
     const userRegistrationTasks = this.generateUserRegistrationTasks(selectn('response.entries', computeUserDialects))
     const userRegistrationTaskList = userRegistrationTasks.length > 0 ? <ul>{userRegistrationTasks}</ul> : null
 
-    const hasTasks = userTasks.length > 0 || userRegistrationTasks.length > 0
+    const hasTasks = (selectn('response', _computeUserTasks) || []).length > 0 || userRegistrationTasks.length > 0
 
     return (
-      <PromiseWrapper renderOnError computeEntities={computeEntities}>
+      <PromiseWrapper
+        renderOnError
+        computeEntities={Immutable.fromJS([
+          {
+            id: userID,
+            entity: this.props.computeUserTasks,
+          },
+          {
+            id: userID,
+            entity: this.props.computeUserDialects,
+          },
+          {
+            id: this.state.lastActionedTaskId,
+            entity: this.props.computeUserTasksReject,
+          },
+        ])}
+      >
         <>
           <h1>{intl.trans('tasks', 'Tasks', 'first')}</h1>
 
           {hasTasks === false && <h2>{intl.trans('views.pages.tasks.no_tasks', 'There are currently No tasks.')}</h2>}
 
           {userRegistrationTaskList}
-          {userTasksTable}
+
+          {hasTasks && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <DictionaryList
+                // Listview: Batch
+                // batchTitleSelect="Deselect all"
+                // batchTitleDeselect="Select all"
+                // batchFooterIsConfirmOrDenyTitle="Delete selected phrase books?"
+                // batchFooterBtnInitiate="Delete"
+                // batchFooterBtnDeny="No, do not delete the selected phrase books"
+                // batchFooterBtnConfirm="Yes, delete the selected phrase books"
+                // batchConfirmationAction={(uids) => {}}
+                // Listview: view mode buttons
+                hasViewModeButtons={false}
+                // Listview: computed data
+                columns={this.getColumns()}
+                computedData={_computeUserTasks}
+                items={selectn('response', _computeUserTasks)}
+                // sortHandler={(sortData) => {}}
+                // Pagination
+                // fetcher={(fetcherParams) => {
+                //   console.log('fetcher', fetcherParams)
+                //   debugger
+                // }}
+                // fetcherParams={{ currentPageIndex: page, pageSize: pageSize }}
+                // hasPagination
+                // metadata={selectn('response', _computeUserTasks)}
+                dictionaryListSmallScreenTemplate={({ templateData }) => {
+                  return (
+                    <span className="DictionaryListSmallScreen__ContributorsListView">
+                      {templateData.documentTitle}
+                      <div>
+                        {templateData.taskName}
+                        {templateData.dueDate}
+                      </div>
+                      {templateData.actions}
+                    </span>
+                  )
+                }}
+              />
+            </Suspense>
+          )}
 
           {hasTasks && (
             <Dialog fullWidth maxWidth="md" open={this.state.open} onClose={this.handleClose}>
@@ -208,55 +219,6 @@ export class Tasks extends React.Component {
     ProviderHelpers.fetchIfMissing(userId, newProps.fetchUserDialects, newProps.computeUserDialects)
   }
 
-  generateUserTasks = (computeUserTasksResponse = []) => {
-    return computeUserTasksResponse.map((task, i) => {
-      return (
-        <TableRow key={i}>
-          <TableCell>
-            <button
-              type="button"
-              className="FlatButton FlatButton--secondary Tasks__taskTitle"
-              onClick={this.handleOpen.bind(this, task.docref)}
-            >
-              {task.documentTitle}
-            </button>
-          </TableCell>
-          <TableCell className="Tasks__taskTypeContainer">
-            <span className="Tasks__taskType">{intl.searchAndReplace(task.name)}</span>
-          </TableCell>
-          <TableCell>
-            <div data-testid="Tasks__approveRejectContainer" className="Tasks__approveRejectContainer">
-              <FVButton
-                variant="contained"
-                color="secondary"
-                onClick={(e) => {
-                  e.preventDefault()
-                  this.handleTaskActions(task.id, 'approve')
-                }}
-              >
-                {intl.trans('approve', 'Approve', 'first')}
-              </FVButton>
-
-              <FVButton
-                variant="contained"
-                color="secondary"
-                onClick={(e) => {
-                  e.preventDefault()
-                  this.handleTaskActions(task.id, 'reject')
-                }}
-              >
-                {intl.trans('reject', 'Reject', 'first')}
-              </FVButton>
-            </div>
-          </TableCell>
-          <TableCell className="Tasks__taskDueDateContainer">
-            <span className="Tasks__taskDueDate">{task.dueDate}</span>
-          </TableCell>
-        </TableRow>
-      )
-    })
-  }
-
   generateUserRegistrationTasks = (computeUserDialectsResponseEntries = []) => {
     return computeUserDialectsResponseEntries.map((dialect, i) => {
       return (
@@ -268,6 +230,129 @@ export class Tasks extends React.Component {
         </li>
       )
     })
+  }
+  /*
+id: "14b1a4e4-4838-4f3e-ad32-1637d671dc27"
+docref: "879a140b-8391-4c46-90da-05773070b7d4"
+name: "Approve/Reject Request to Enable"
+taskName: "Approve/Reject Request to Enable"
+directive: "Approval to Enable required"
+comment: ""
+dueDate: "2020-02-13T00:45:29.116Z"
+documentTitle: "WORD"
+documentLink: "nxpath/default/FV/Workspaces/Data/Test/Test/TestLanguageOne/Dictionary/1581551249254@view_documents"
+startDate: "2020-02-13T00:45:29.120Z"
+expired: true
+*/
+  getColumns = () => {
+    return [
+      {
+        name: 'documentTitle',
+        title: intl.trans('document_title', 'Document Title', 'words'),
+        columnDataTemplate: dictionaryListSmallScreenColumnDataTemplate.cellRenderTypography,
+        render: (v, data) => {
+          return (
+            <FVButton
+              variant="outlined"
+              color="secondary"
+              className="Tasks__taskTitle"
+              onClick={this.handleOpen.bind(this, data.docref)}
+            >
+              {v}
+            </FVButton>
+          )
+        },
+        // sortBy: 'documentTitle',
+      },
+      {
+        name: 'taskName',
+        title: intl.trans('task_type', 'Task Type', 'words'),
+        render: (v) => {
+          return <span className="Tasks__taskType">{intl.searchAndReplace(v)}</span>
+        },
+        // sortBy: 'taskName',
+      },
+      {
+        name: 'taskActions',
+        title: intl.trans('actions', 'Actions', 'words'),
+        render: (v, data) => {
+          return (
+            <div data-testid="Tasks__approveRejectContainer" className="Tasks__approveRejectContainer">
+              <FVButton
+                className="Tasks__approve"
+                variant="contained"
+                color="secondary"
+                onClick={(e) => {
+                  e.preventDefault()
+                  this.handleTaskActions(data.id, 'approve')
+                }}
+              >
+                {intl.trans('approve', 'Approve', 'first')}
+              </FVButton>
+
+              <FVButton
+                className="Tasks__reject"
+                variant="contained"
+                color="secondary"
+                onClick={(e) => {
+                  e.preventDefault()
+                  this.handleTaskActions(data.id, 'reject')
+                }}
+              >
+                {intl.trans('reject', 'Reject', 'first')}
+              </FVButton>
+            </div>
+          )
+        },
+      },
+      {
+        name: 'dueDate',
+        title: intl.trans('task_due_date', 'Task Due Date', 'words'),
+        render: (v) => {
+          return StringHelpers.formatUTCDateString(v)
+        },
+        // sortBy: 'dueDate',
+      },
+      // {
+      //   name: 'actions',
+      //   title: 'ACTIONS',
+      //   columnDataTemplate: dictionaryListSmallScreenColumnDataTemplate.cellRender,
+      //   render: (v, data) => {
+      //     const uid = data.uid
+      //     const url = `/${siteTheme}${dialect_path}/edit/phrasebook/${uid}`
+
+      //     return (
+      //       <ul className="Phrasebooks__actions">
+      //         <li className="Phrasebooks__actionContainer Phrasebooks__actionDelete">
+      //           <ConfirmationDelete
+      //             reverse
+      //             compact
+      //             copyIsConfirmOrDenyTitle={copy.isConfirmOrDenyTitle}
+      //             copyBtnInitiate={copy.btnInitiate}
+      //             copyBtnDeny={copy.btnDeny}
+      //             copyBtnConfirm={copy.btnConfirm}
+      //             confirmationAction={() => {
+      //               props.deleteCategory(uid)
+      //               setDeletedUids([...deletedUids, uid])
+      //             }}
+      //           />
+      //         </li>
+      //         <li className="Phrasebooks__actionContainer">
+      //           <a
+      //             href={url}
+      //             onClick={(e) => {
+      //               e.preventDefault()
+      //               NavigationHelpers.navigate(url, props.pushWindowPath, false)
+      //             }}
+      //           >
+      //             {copy.actions.edit}
+      //           </a>
+      //         </li>
+      //       </ul>
+      //     )
+      //   },
+      // },
+    ]
   }
 
   handleClose = () => {
