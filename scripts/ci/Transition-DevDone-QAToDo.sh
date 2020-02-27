c1d3be7b5933fae757818e9e952b6aeb11c0d732
Nolan Van Hell <nolan@fpcc.ca>
2020-02-26 16:48:57 -0800

    FW-688 Add all completed transitions and scripts

--- scripts/ci/Transition-DevDone-QAToDo.sh
+++ scripts/ci/Transition-DevDone-QAToDo.sh
@@ -9,7 +9,7 @@ COMMITMESSAGES=$1
 # Replace commas with spaces for pattern matching
 COMMITMESSAGES=${COMMITMESSAGES//,/ }
 
-# Remove all items matching the patter from the list and append them to FILTEREDLIST
+# Remove all items matching the pattern from the list and append them to FILTEREDLIST
 FILTEREDLIST=""
 for f in $COMMITMESSAGES
 do
@@ -22,21 +22,45 @@ done
 FILTEREDLIST="$(echo $FILTEREDLIST | tr ' ' '\n' | sort | uniq | xargs)"
 
 # Iterate through each issue found in the commit messages and perform Jira actions on each
+echo ''
 for f in $FILTEREDLIST
 do
     if [[ $f =~ FW-[0-9]{1,5} ]]; then
-        echo ${BASH_REMATCH}
-        echo "Transitioning - DEV DONE -> QA TO DO: " ${BASH_REMATCH}
-        jira transition --noedit "QA TO DO" ${BASH_REMATCH}
-        echo ""
-        echo "Unassigning issue: " ${BASH_REMATCH}
-        jira unassign --default ${BASH_REMATCH}
-        echo ""
-        echo "Adding DEPLOYED-DEV label to issue:" ${BASH_REMATCH}
-        jira labels add ${BASH_REMATCH} DEPLOYED-DEV
-        echo ""
-        echo "Adding comment to issue:" ${BASH_REMATCH}
-        jira comment --noedit --comment="This issue has been merged to master and will be deployed to dev.firstvoices.com automatically." ${BASH_REMATCH}
-        echo ""
+        ISSUE=${BASH_REMATCH}
+        FOUND=false
+        jira view $ISSUE > /dev/null 2>&1
+        if [[ $? -eq 0 ]]; then
+            FOUND=true
+        fi
+        if [[ $FOUND = true ]]; then
+            STATUS=false
+            INFO=$(jira view ${BASH_REMATCH})
+            if [[ $(echo $INFO | grep -o 'status:[[:space:]].*[[:space:]]summary:') =~ Dev[[:space:]]Done ]]; then
+                STATUS=true
+            fi
+            if [[ $STATUS = true ]]; then
+                echo $ISSUE ": Transitioning - DEV DONE -> QA TO DO."
+                jira transition --noedit "QA TO DO" $ISSUE
+                echo $ISSUE ": Unassigning issue."
+                jira unassign $ISSUE
+                echo $ISSUE ": Adding DEPLOYED-DEV label to issue."
+                jira labels add $ISSUE DEPLOYED-DEV
+                echo $ISSUE ": Adding comment to issue."
+                jira comment --noedit --comment="This issue has been merged to master and will be deployed to dev.firstvoices.com automatically." $ISSUE
+            else
+                echo $ISSUE ": No transition performed. Issue is not in the DEV DONE status."
+            fi
+        else
+            echo $ISSUE ": No transition performed. Issue not found on jira."
+        fi
     fi
+    if [[ $ISSUESFOUND == 0 ]]; then
+        echo 'No issues in commit messages to transition.'
+    fi
+    echo ''
+    echo ''
 done
+if [[ $FILTEREDLIST = '' ]]; then
+    echo 'No issue key found to transition.'
+fi
+echo ''
--- scripts/ci/Transition-DevInProgress-DevDone.sh
+++ scripts/ci/Transition-DevInProgress-DevDone.sh
@@ -4,14 +4,16 @@
 # DEV IN PROGRESS to DEV DONE
 
 # Save commit messages to the variable
-COMMITMESSAGES=$1
+PRTITLE=$1
+URL=$2
+PRNUM=$3
 
 # Replace commas with spaces for pattern matching
-COMMITMESSAGES=${COMMITMESSAGES//,/ }
+PRTITLE=${PRTITLE//,/ }
 
-# Remove all items matching the patter from the list and append them to FILTEREDLIST
+# Remove all items matching the pattern from the list and append them to FILTEREDLIST
 FILTEREDLIST=""
-for f in $COMMITMESSAGES
+for f in $PRTITLE
 do
     if [[ $f =~ FW-[0-9]{1,5} ]]; then
         FILTEREDLIST="$FILTEREDLIST ${BASH_REMATCH}"
@@ -21,13 +23,58 @@ done
 # Filter out any duplicates
 FILTEREDLIST="$(echo $FILTEREDLIST | tr ' ' '\n' | sort | uniq | xargs)"
 
-# Iterate through each issue found in the commit messages and perform Jira actions on each
+# Get commit messages and combine with the pr title
+COMMITS=$(curl -s https://api.github.com/repos/$URL/pulls/$PRNUM/commits | jq '.[].commit.message')
+FILTEREDLIST="$FILTEREDLIST $COMMITS"
+
+# Replace commas and quotation marks with spaces for easy pattern matching
+FILTEREDLIST=${FILTEREDLIST//,/ }
+FILTEREDLIST=${FILTEREDLIST//\"/ }
+
+# Remove all items matching the pattern FW-XXXXX from the list and append them to KEYLIST
+KEYLIST=""
 for f in $FILTEREDLIST
 do
     if [[ $f =~ FW-[0-9]{1,5} ]]; then
-        echo ${BASH_REMATCH}
-        echo "Transitioning - DEV IN PROGRESS -> DEV DONE: " ${BASH_REMATCH}
-        jira transition --noedit "DEV DONE" ${BASH_REMATCH}
-        echo ""
+        KEYLIST="$KEYLIST ${BASH_REMATCH}"
     fi
 done
+
+# Filter out any duplicates
+KEYLIST="$(echo $KEYLIST | tr ' ' '\n' | sort | uniq | xargs)"
+echo 'KEYLIST:'
+echo $KEYLIST
+echo''
+# Iterate through each issue found in the commit messages and perform Jira actions on each
+for f in $KEYLIST
+do
+    if [[ $f =~ FW-[0-9]{1,5} ]]; then
+        ISSUE=${BASH_REMATCH}
+        FOUND=false
+        jira view $ISSUE > /dev/null 2>&1
+        if [[ $? -eq 0 ]]; then
+            FOUND=true
+        fi
+        if [[ $FOUND = true ]]; then
+            STATUS=false
+            INFO=$(jira view $ISSUE)
+            if [[ $(echo $INFO | grep -o 'status:[[:space:]].*[[:space:]]summary:') =~ Dev[[:space:]]In[[:space:]]Progress ]]; then
+                STATUS=true
+            fi
+            if [[ $STATUS = true ]]; then
+                echo $ISSUE ": Transitioning - DEV IN PROGRESS -> DEV DONE."
+                jira transition --noedit "DEV DONE" $ISSUE
+                echo ""
+            else
+                echo $ISSUE ": No transition performed. Issue is not in the DEV IN PROGRESS status."
+            fi
+        else
+            echo $ISSUE ": No transition performed. Issue not found on jira."
+        fi
+        echo ''
+    fi
+done
+if [[ $KEYLIST = '' ]]; then
+    echo 'No issue key found to transition.'
+fi
+echo ''
