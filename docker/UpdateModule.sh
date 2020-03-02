167cbc8c8cd8f3a4b64617e1ac778794eb3659b5
Nolan Van Hell <nolan@fpcc.ca>
2020-03-02 10:05:39 -0800

    FW-1016 Create initial UpdateModule.sh script

---
+++ docker/UpdateModule.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DIRECTORY=$PWD
+MODULE=$1
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+cd $DIRECTORY/$1
+
+# Build main project.
+echo 'Building module: ' $1
+cd ..
+mvn clean install
+if [[ "$?" -ne 0 ]]; then
+    echo
+    echo -e "${RED}fv-web-ui build failed \n${ENDCOLOR}"; exit 1
+    echo
+fi
+echo
+
+echo 'Removing old bundle from docker container if it exists.'
+docker exec nuxeo-dev rm /opt/nuxeo/server/nxserver/bundles/$1-*.jar
+if [[ "$?" -ne 0 ]]; then
+    echo
+    echo -e "${RED}Removal of old bundles failed. \n${ENDCOLOR}"; exit 1
+    echo
+fi
+echo
+
+echo 'Copying new bundle into docker container and restarting nuxeo backend.'
+docker cp target/$1-*.jar nuxeo-dev:/opt/nuxeo/server/nxserver/bundles/ && docker exec nuxeo-dev nuxeoctl restart
+if [[ "$?" -ne 0 ]]; then
+    echo
+    echo -e "${RED}Copy new bundle build into docker failed. \n${ENDCOLOR}"; exit 1
+    echo
+fi
+echo
+
+echo
+echo -e "-----------------------------------------------------------------"
+echo -e "${GREEN}Module built and copied into the docker container successfully.${ENDCOLOR}"
+echo -e "-----------------------------------------------------------------"
+exit 0
