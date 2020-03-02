42d6b6e23c494b2b44adc9a1ea01cb0e66b5cda0
Nolan Van Hell <nolan@fpcc.ca>
2020-03-02 11:08:08 -0800

    FW-1016 Add complete UpdateModule.sh script

--- docker/UpdateModule.sh
+++ docker/UpdateModule.sh
@@ -7,27 +7,21 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-cd $DIRECTORY/$1
-
+cd $DIRECTORY/../$1
+ls
 # Build main project.
 echo 'Building module: ' $1
-cd ..
 mvn clean install
 if [[ "$?" -ne 0 ]]; then
     echo
     echo -e "${RED}fv-web-ui build failed \n${ENDCOLOR}"; exit 1
     echo
 fi
-echo
+echo ''
 
 echo 'Removing old bundle from docker container if it exists.'
-docker exec nuxeo-dev rm /opt/nuxeo/server/nxserver/bundles/$1-*.jar
-if [[ "$?" -ne 0 ]]; then
-    echo
-    echo -e "${RED}Removal of old bundles failed. \n${ENDCOLOR}"; exit 1
-    echo
-fi
-echo
+docker exec nuxeo-dev sh -c 'rm /opt/nuxeo/server/nxserver/bundles/$1-*.jar'
+echo ''
 
 echo 'Copying new bundle into docker container and restarting nuxeo backend.'
 docker cp target/$1-*.jar nuxeo-dev:/opt/nuxeo/server/nxserver/bundles/ && docker exec nuxeo-dev nuxeoctl restart
@@ -36,10 +30,10 @@ if [[ "$?" -ne 0 ]]; then
     echo -e "${RED}Copy new bundle build into docker failed. \n${ENDCOLOR}"; exit 1
     echo
 fi
-echo
+echo ''
 
-echo
-echo -e "-----------------------------------------------------------------"
+echo ''
+echo -e "---------------------------------------------------------------"
 echo -e "${GREEN}Module built and copied into the docker container successfully.${ENDCOLOR}"
-echo -e "-----------------------------------------------------------------"
+echo -e "---------------------------------------------------------------"
 exit 0
