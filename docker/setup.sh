d5042076af87bff511f0a072ace1b7775aec8e38
Nolan Van Hell <nolan@fpcc.ca>
2020-03-03 20:45:14 -0800

    FW-672 Automate the call to FVInitialDatabaseSetup on backend start

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,4 +3,21 @@ nuxeoctl mp-install --accept=yes /opt/nuxeo/server/nxserver/tmp/FirstVoices-mark
 touch /opt/nuxeo/server/nxserver/tmp/this.folder.is.mounted.in.docker.container
 
 # This will exec the CMD from your Dockerfile, i.e. "npm start"
+
+# Startup a process in the background which checks for the server startup
+ # every ten seconds and runs the initial setup script if it has started.
+(
+    STARTED=0
+    echo $STARTED
+    while [[ $STARTED -ne 1 ]]
+    do
+        if [[ $(nuxeoctl status) =~ Server[[:space:]]is[[:space:]]running[[:space:]]with[[:space:]]process[[:space:]]ID ]]; then
+            STARTED=1
+        fi
+        sleep 10
+    done
+    /opt/nuxeo/server/tmp/initialsetup.sh; exit 0
+) & disown
+
+# Startup the backend
 exec "$@"
