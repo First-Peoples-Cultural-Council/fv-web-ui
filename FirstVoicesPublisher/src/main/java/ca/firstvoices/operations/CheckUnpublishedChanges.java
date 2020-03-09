06009eceaedce8f8018efaa154de0463b660ee83
Nolan Van Hell <nolan@fpcc.ca>
2020-03-09 14:51:26 -0700

    FW-936 FW-1046 Add service and operation to check for unpublished changes

---
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/operations/CheckUnpublishedChanges.java
@@ -0,0 +1,28 @@
+package ca.firstvoices.operations;
+
+import ca.firstvoices.services.UnpublishedChangesService;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ *
+ */
+@Operation(id=CheckUnpublishedChanges.ID, category= Constants.CAT_DOCUMENT, label="FVCheckUnpublishedChanges",
+        description="This operation is used to check if a document has unpublished changes. " +
+                "It returns true if unpublished changes exist and false otherwise.")
+public class CheckUnpublishedChanges {
+
+    public static final String ID = "Document.CheckUnpublishedChanges";
+
+    protected UnpublishedChangesService service = Framework.getService(UnpublishedChangesService.class);
+
+    @OperationMethod
+    public boolean run(DocumentModel input) {
+        // Call the checkUnpublishedChanges service which returns true if a document has unpublished changes and false otherwise.
+        return service.checkUnpublishedChanges(input);
+    }
+
+}
---
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/services/UnpublishedChangesServiceImpl.java
@@ -0,0 +1,95 @@
+package ca.firstvoices.services;
+
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.schema.DocumentType;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.CoreSession;
+
+import java.util.Arrays;
+
+public class UnpublishedChangesServiceImpl implements UnpublishedChangesService {
+
+    public boolean checkUnpublishedChanges(DocumentModel document) {
+
+        CoreSession session = document.getCoreSession();
+
+        // Check that the document is a specific type using the helper method
+        if (!(checkType(document)))
+            return false;
+
+        // Get the Workspaces document
+        String path = document.getPathAsString();
+        String[] splitDocPath = path.split("/");
+        splitDocPath[2] = "Workspaces";
+        path = String.join("/", splitDocPath);
+        DocumentModel workspacesDocument = session.getDocument((new PathRef(path)));
+
+        // Get the workspaces version and status of the document
+        int majorVerDoc = Integer.parseInt(workspacesDocument.getPropertyValue("uid:major_version").toString());
+        int minorVerDoc = Integer.parseInt(workspacesDocument.getPropertyValue("uid:minor_version").toString());
+        String status = workspacesDocument.getCurrentLifeCycleState();
+
+        // Check that the document is currently published
+        if (! status.equals("Published")) {
+            return false;
+        }
+
+        // Get the sections document
+        String[] splitPath = path.split("/");
+        splitPath[2] = "sections";
+        String sectionsPath = String.join("/", splitPath);
+        DocumentModel sectionsDocument = session.getDocument(new PathRef(sectionsPath));
+
+        // Get the sections version
+        int majorVerSections = Integer.parseInt(sectionsDocument.getPropertyValue("uid:major_version").toString());
+        int minorVerSections = Integer.parseInt(sectionsDocument.getPropertyValue("uid:minor_version").toString());
+
+        /*
+            Use the helper method to compare versions of the document and return true if the sections
+            version is older then the workspaces version.
+         */
+        return compareVersions(majorVerDoc, minorVerDoc, majorVerSections, minorVerSections);
+
+    }
+
+    // Helper method to check if the workspaces document version is greater then then sections document version
+    private boolean compareVersions(int majorVerDoc, int minorVerDoc, int majorVerSections, int minorVerSections) {
+        if (majorVerDoc > majorVerSections) {
+            return true;
+        } else return majorVerDoc == majorVerSections && minorVerDoc > minorVerSections;
+    }
+
+    // Helper method to check that the new document is one of the types below
+    private boolean checkType(DocumentModel inputDoc) {
+        DocumentType currentType = inputDoc.getDocumentType();
+
+        String[] types = {
+                "FVAlphabet",
+                "FVAudio",
+                "FVBook",
+                "FVBookEntry",
+                "FVBooks",
+                "FVCategories",
+                "FVCategory",
+                "FVCharacter",
+                "FVContributor",
+                "FVContributors",
+                "FVDialect",
+                "FVDictionary",
+                "FVGallery",
+                "FVLanguage",
+                "FVLanguageFamily",
+                "FVLink",
+                "FVLinks",
+                "FVPhrase",
+                "FVPicture",
+                "FVPortal",
+                "FVResources",
+                "FVVideo",
+                "FVWord",
+        };
+
+        return Arrays.stream(types).parallel().anyMatch(currentType.toString()::contains);
+    }
+
+}
