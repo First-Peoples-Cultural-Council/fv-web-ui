602481a0f0f12a5fd77d97f84b94ce79175393e4
davidlacho <david.lacho@gmail.com>
2020-03-10 13:06:24 -0700

    FV-1009: Unit tests refactors

--- FirstVoicesPublisher/src/test/java/ca/firstvoices/listeners/FirstVoicesNativeOrderTest.java
+++ FirstVoicesPublisher/src/test/java/ca/firstvoices/listeners/FirstVoicesNativeOrderTest.java
@@ -39,12 +39,13 @@ import ca.firstvoices.nativeorder.services.NativeOrderComputeService;
  */
 @RunWith(FeaturesRunner.class)
 @Features({RuntimeFeature.class, CoreFeature.class, PlatformFeature.class})
-@Deploy({"FirstVoicesData", "org.nuxeo.ecm.platform", "org.nuxeo.ecm.platform.commandline.executor", "org.nuxeo.ecm" +
-        ".platform.picture.core", "org.nuxeo.ecm.platform.rendition.core", "org.nuxeo.ecm.platform" + ".video.core",
-        "org.nuxeo.ecm.platform.audio.core", "org.nuxeo.ecm.automation.scripting", "FirstVoicesNuxeoPublisher" +
-        ".tests:OSGI-INF/extensions/ca.firstvoices.fakestudio.xml", "FirstVoicesNuxeoPublisher:OSGI-INF/extensions/ca" +
-        ".firstvoices.templates.factories.xml", "FirstVoicesSecurity:OSGI-INF/extensions/ca.firstvoices.operations" +
-        ".xml", "FirstVoicesNuxeoPublisher:OSGI" + "-INF/extensions/ca.firstvoices.nativeorder.services.xml"})
+@Deploy({"FirstVoicesData", "org.nuxeo.ecm.platform", "org.nuxeo.ecm.platform.commandline.executor",
+        "org.nuxeo.ecm" + ".platform.picture.core", "org.nuxeo.ecm.platform.rendition.core",
+        "org.nuxeo.ecm.platform" + ".video.core", "org.nuxeo.ecm.platform.audio.core", "org.nuxeo.ecm.automation" +
+        ".scripting", "FirstVoicesNuxeoPublisher" + ".tests:OSGI-INF/extensions/ca.firstvoices.fakestudio.xml",
+        "FirstVoicesNuxeoPublisher:OSGI-INF/extensions/ca" + ".firstvoices.templates.factories.xml",
+        "FirstVoicesSecurity:OSGI-INF/extensions/ca.firstvoices.operations" + ".xml", "FirstVoicesNuxeoPublisher:OSGI"
+        + "-INF/extensions/ca.firstvoices.nativeorder.services.xml"})
 public class FirstVoicesNativeOrderTest {
     @Inject
     protected CoreSession session;
@@ -80,13 +81,14 @@ public class FirstVoicesNativeOrderTest {
                 "tl'", "ts", "ts'", "u", "uu", "w", "w̓", "x", "x̱", "xw", "y", "y̓", "'"};
 
         createAlphabet(orderedAlphabet, "/Family/Language/Dialect/Alphabet");
-        createWords(orderedWords, "/Family/Language/Dialect/Dictionary");
+        createWordsorPhrases(orderedWords, "FVWord");
 
         nativeOrderComputeService.computeDialectNativeOrderTranslation(dialect);
         Integer i = orderedWords.length - 1;
 
-        DocumentModelList docs = session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "' " +
-                "ORDER BY " + "fv:custom_order DESC");
+        DocumentModelList docs =
+                session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "' " + "ORDER BY " +
+                        "fv:custom_order DESC");
 
         for (DocumentModel doc : docs) {
             String reference = (String) doc.getPropertyValue("fv:reference");
@@ -105,19 +107,20 @@ public class FirstVoicesNativeOrderTest {
                 "piišpiš", "qacc̕a", "qiicqiica", "qiišʔaqƛi", "sasin", "saasin", "suč’a", "šuuwis", "t̓iqʷas",
                 "uksuukł", "ʔukłaa", "ʕuupqšiƛ", "weʔičʔin", "wiwiiquk", "xʷakak", "yaciicʔił", "yeeł", "y̕eʔisi"};
 
-        String[] orderedAlphabet = {"a", "ʔa", "ʕa", "aa", "ʔaa", "ʕaa", "e", "ʔe", "ʕe", "ee", "ʔee", "ʕee", "c", "c" +
-                "̕", "č", "č’", "h", "ḥ", "i", "ʔi", "ʕi", "ii", "ʔii", "ʕii", "k", "k̕", "kʷ", "k̕ʷ", "ł", "ƛ",
+        String[] orderedAlphabet = {"a", "ʔa", "ʕa", "aa", "ʔaa", "ʕaa", "e", "ʔe", "ʕe", "ee", "ʔee", "ʕee", "c", "c"
+                + "̕", "č", "č’", "h", "ḥ", "i", "ʔi", "ʕi", "ii", "ʔii", "ʕii", "k", "k̕", "kʷ", "k̕ʷ", "ł", "ƛ",
                 "ƛ̕", "m", "m̕", "n", "n̕", "p", "p̕", "q", "qʷ", "s", "š", "t", "t̕", "u", "ʔu", "ʕu", "uu", "ʔuu",
                 "ʕuu", "w", "w̕", "x", "x̣", "xʷ", "x̣ʷ", "y", "y̕", "ʕ", "ʔ"};
 
         createAlphabet(orderedAlphabet, "/Family/Language/Dialect/Alphabet");
-        createWords(orderedWords, "/Family/Language/Dialect/Dictionary");
+        createWordsorPhrases(orderedWords, "FVWord");
 
         nativeOrderComputeService.computeDialectNativeOrderTranslation(dialect);
         Integer i = orderedWords.length - 1;
 
-        DocumentModelList docs = session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "' " +
-                "ORDER BY " + "fv:custom_order DESC");
+        DocumentModelList docs =
+                session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "' " + "ORDER BY " +
+                        "fv:custom_order DESC");
 
         for (DocumentModel doc : docs) {
             String reference = (String) doc.getPropertyValue("fv:reference");
@@ -136,8 +139,6 @@ public class FirstVoicesNativeOrderTest {
                 "adjustment", "advertisement", "after", "again", "against", "agreement", "air", "all", "almost",
                 "among", "amount", "baby", "back", "bad", "bag", "balance", "ball", "band", "cake", "camera", "canvas"
                 , "card", "care", "carriage", "damage", "danger", "dark", "daughter", "day", "dead", "dear", "death",
-                "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction",
-                "detail", "development", "different", "digestion", "direction", "discovery", "discussion", "disease",
                 "disgust", "distance", "distribution", "ear", "early", "earth", "east", "edge", "education", "effect"
                 , "egg", "elastic", "electric", "end", "face", "fact", "fall", "false", "family", "far", "farm", "fat"
                 , "father", "fear", "feather", "glass", "glove", "go", "goat", "gold", "good", "government", "grain",
@@ -145,25 +146,14 @@ public class FirstVoicesNativeOrderTest {
                 , "hanging", "happy", "harbor", "ink", "insect", "instrument", "insurance", "interest", "invention",
                 "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "key",
                 "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge, land", "language", "last", "late", "map"
-                , "mark", "market", "married", "match", "material", "may", "meal", "measure", "meat", "medical",
-                "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed"
                 , "money", "note", "now", "number", "nut", "observation", "of", "off", "offer", "office", "oil", "old"
                 , "on", "only", "open", "paste", "payment", "peace", "pen", "pencil", "person", "physical", "picture"
-                , "pig", "pin", "pipe", "place", "prose", "protest", "public", "pull", "purpose", "push", "put, " +
-                "quality", "question", "quick", "quiet", "quite, rail", "rain", "range", "rat", "rate", "ray",
-                "reaction", "river", "road", "rod", "roll", "roof", "room", "root", "rough", "round", "rub", "rule",
+                , "quality", "question", "quick", "quiet", "quite, rail", "rain", "range", "rat", "rate", "ray",
                 "run, sad", "safe", "sail", "salt", "same", "sand", "say", "scale", "school", "science", "scissors",
-                "skirt", "sky", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke",
-                "smooth", "snake", "sneeze", "snow", "so", "soap", "society", "sock", "soft", "solid", "some", "stick"
-                , "sticky", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight",
                 "strange", "street", "strong", "structure", "substance", "such", "theory", "there", "thick", "thin",
-                "thing", "this", "though", "thought", "thread", "throat", "through", "thumb", "thunder", "ticket",
                 "tight", "till", "time", "tin", "tired", "to", "toe", "together", "unit", "up", "use, value", "verse"
-                , "very", "vessel", "view", "violent", "voice, waiting", "walk", "wall", "war", "warm", "wash",
-                "waste", "watch", "water", "wave", "wax", "way", "weather", "week", "weight", "well", "west", "wet",
-                "wheel", "when", "where", "while", "whip", "whistle", "white", "who", "why", "wide", "will", "wind",
-                "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work",
-                "worm", "wound", "writing", "wrong", "year", "yellow", "yes", "yesterday", "you", "young", " "};
+                , "very", "vessel", "view", "violent", "voice, waiting", "walk", "wall", "war", "warm", "wash", "worm"
+                , "wound", "writing", "wrong", "year", "yellow", "yes", "yesterday", "you", "young", " "};
 
         String[] orderedAlphabet = {"À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï",
                 "Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "×", "Ø", "Ù", "Ú", "Û", "Ü", "Ý", "Þ", "ß", "à", "á", "â", "ã",
@@ -171,13 +161,14 @@ public class FirstVoicesNativeOrderTest {
                 "ø", "ù", "ú", "û", "ü", "ý", "þ", "ÿ"};
 
         createAlphabet(orderedAlphabet, "/Family/Language/Dialect/Alphabet");
-        createWords(orderedWords, "/Family/Language/Dialect/Dictionary");
+        createWordsorPhrases(orderedWords, "FVWord");
 
         nativeOrderComputeService.computeDialectNativeOrderTranslation(dialect);
         Integer i = orderedWords.length - 1;
 
-        DocumentModelList docs = session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "' " +
-                "ORDER BY " + "fv:custom_order DESC");
+        DocumentModelList docs =
+                session.query("SELECT * FROM FVWord WHERE ecm:ancestorId='" + dialect.getId() + "' " + "ORDER BY " +
+                        "fv:custom_order DESC");
 
         for (DocumentModel doc : docs) {
             String reference = (String) doc.getPropertyValue("fv:reference");
@@ -194,14 +185,14 @@ public class FirstVoicesNativeOrderTest {
                 "ä", "å", "æ", "ç", "è", "é", "ê", "ë", "ì", "í", "î", "ï", "ð", "ñ", "ò", "ó", "ô", "õ", "ö", "÷",
                 "ø", "ù", "ú", "û", "ü", "ý", "þ", "ÿ", "A bad excuse is better than none", "Better safe than sorry",
                 "Curiosity killed the cat", "Do not make a mountain out of a mole " + "hill", "Easy come, easy go",
-                "Fine feathers make fine birds", "Give credit where credit is due", "Home is where the heart is", "If" +
-                " you play with fire, you will get burned", "Judge not, that " + "ye be not judged", "Kill two birds " +
-                "with one stone.", "Learn a language, and you will avoid a " + "war", "Memory is the treasure of the " +
-                "mind", "No man is an island", "Oil and water do not mix", "Penny, Penny. Makes many.", "Respect is " +
-                "not given, it is earned.", "Sticks and stones may break" + " my bones, but words will never hurt me" +
-                ".", "There is no smoke without fire.", "Use it or lose it", "Virtue is its own reward", "When it " +
-                "rains it pours.", "You cannot teach an old dog new tricks", "Zeal without knowledge is fire without " +
-                "light.", " "};
+                "Fine feathers make fine birds", "Give credit where credit is due", "Home is where the heart is", "If"
+                + " you play with fire, you will get burned", "Judge not, that " + "ye be not judged", "Kill two " +
+                "birds " + "with one stone.", "Learn a language, and you will avoid a " + "war", "Memory is the " +
+                "treasure of the " + "mind", "No man is an island", "Oil and water do not mix", "Penny, Penny. Makes " +
+                "many.", "Respect is " + "not given, it is earned.", "Sticks and stones may break" + " my bones, but " +
+                "words will never hurt me" + ".", "There is no smoke without fire.", "Use it or lose it", "Virtue is " +
+                "its own reward", "When it " + "rains it pours.", "You cannot teach an old dog new tricks", "Zeal " +
+                "without knowledge is fire without " + "light.", " "};
 
         String[] orderedAlphabet = {"À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï",
                 "Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "×", "Ø", "Ù", "Ú", "Û", "Ü", "Ý", "Þ", "ß", "à", "á", "â", "ã",
@@ -209,13 +200,13 @@ public class FirstVoicesNativeOrderTest {
                 "ø", "ù", "ú", "û", "ü", "ý", "þ", "ÿ"};
 
         createAlphabet(orderedAlphabet, "/Family/Language/Dialect/Alphabet");
-        createPhrases(orderedPhrases, "/Family/Language/Dialect/Dictionary");
+        createWordsorPhrases(orderedPhrases, "FVPhrase");
 
         nativeOrderComputeService.computeDialectNativeOrderTranslation(dialect);
         Integer i = orderedPhrases.length - 1;
 
-        DocumentModelList docs = session.query("SELECT * FROM FVPhrase WHERE ecm:ancestorId='" + dialect.getId() + "'" +
-                " ORDER BY fv:custom_order DESC");
+        DocumentModelList docs = session.query("SELECT * FROM FVPhrase WHERE ecm:ancestorId='" + dialect.getId() +
+                "'" + " ORDER BY fv:custom_order DESC");
 
         for (DocumentModel doc : docs) {
             String reference = (String) doc.getPropertyValue("fv:reference");
@@ -240,42 +231,21 @@ public class FirstVoicesNativeOrderTest {
         }
     }
 
-    private DocumentModel createWord(String dictionaryPath, String wordValue, String pv, String v) {
-        DocumentModel word = session.createDocumentModel(dictionaryPath, wordValue, "FVWord");
+    private DocumentModel createWordorPhrase(String value, String typeName, String pv, String v) {
+        DocumentModel document = session.createDocumentModel("/Family/Language/Dialect/Dictionary", value, typeName);
         if (pv != null) {
-            word.setPropertyValue(pv, v);
+            document.setPropertyValue(pv, v);
         }
 
-        word = createDocument(word);
+        document = createDocument(document);
 
-        return word;
+        return document;
     }
 
-    private DocumentModel createPhrase(String dictionaryPath, String phraseValue, String pv, String v) {
-        DocumentModel phrase = session.createDocumentModel(dictionaryPath, phraseValue, "FVPhrase");
-        if (pv != null) {
-            phrase.setPropertyValue(pv, v);
-        }
-
-        phrase = createDocument(phrase);
-
-        return phrase;
-    }
-
-    private void createWords(String[] orderedWords, String dictionaryPath) {
+    private void createWordsorPhrases(String[] orderedValues, String typeName) {
         Integer i = 0;
-        for (String wordValue : orderedWords) {
-            createWord(dictionaryPath, wordValue, "fv:reference", String.valueOf(i));
-            i++;
-        }
-
-        session.save();
-    }
-
-    private void createPhrases(String[] orderedPhrases, String dictionaryPath) {
-        Integer i = 0;
-        for (String wordValue : orderedPhrases) {
-            createPhrase(dictionaryPath, wordValue, "fv:reference", String.valueOf(i));
+        for (String value : orderedValues) {
+            createWordorPhrase(value, typeName, "fv:reference", String.valueOf(i));
             i++;
         }
 
