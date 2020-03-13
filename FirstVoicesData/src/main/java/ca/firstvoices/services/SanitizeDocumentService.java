7afafa91e81a322a237f748b0e2ba55336277799
JaredMLarter <jared@fpcc.ca>
2020-03-13 09:36:22 -0700

    FW-1007 - added whitespace trimming service

--- FirstVoicesData/src/main/java/ca/firstvoices/services/SanitizeDocumentService.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/services/SanitizeDocumentService.java
@@ -1,10 +1,10 @@
-// package ca.firstvoices.services;
+package ca.firstvoices.services;
 
-// import org.nuxeo.ecm.core.api.CoreSession;
-// import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
 
-// public interface SanitizeDocumentService {
+public interface SanitizeDocumentService {
   
-//   public void sanitizeDocument(CoreSession session, DocumentModel currentDoc);
+  public void sanitizeDocument(CoreSession session, DocumentModel currentDoc);
   
-// }
+}
--- FirstVoicesData/src/main/java/ca/firstvoices/services/SanitizeDocumentServiceImpl.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/services/SanitizeDocumentServiceImpl.java
@@ -1,15 +1,29 @@
-// package ca.firstvoices.services;
+package ca.firstvoices.services;
 
-// import org.nuxeo.ecm.core.api.CoreSession;
-// import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
 
-// public class SanitizeDocumentServiceImpl implements SanitizeDocumentService {
+public class SanitizeDocumentServiceImpl implements SanitizeDocumentService {
   
-//     public void sanitizeDocument(CoreSession session, DocumentModel currentDoc) {
-//         String title = currentDoc.getTitle();
+    //Sanitizes the document via various functions, and saves the document if edited
+    public void sanitizeDocument(CoreSession session, DocumentModel currentDoc) {
+        boolean edited = false;
+        if((currentDoc.getType().equals("FVWord") || currentDoc.getType().equals("FVPhrase")) 
+            && !currentDoc.isProxy()){
+                edited = edited || trimSpaces(currentDoc);       
+        }
+        
+        if(edited) session.saveDocument(currentDoc);
+    }
 
-//         currentDoc.setPropertyValue("dc:title", title.trim());
-//         session.saveDocument(currentDoc);
+    //Trims spaces if any spaces need to be trimmed
+    private Boolean trimSpaces(DocumentModel currentDoc){
 
-//     }
-// }
+        String title = currentDoc.getTitle();
+        if(!title.equals(title.trim())){        
+            currentDoc.setPropertyValue("dc:title", title.trim());
+            return true;
+        }
+        return false;
+    }
+}
