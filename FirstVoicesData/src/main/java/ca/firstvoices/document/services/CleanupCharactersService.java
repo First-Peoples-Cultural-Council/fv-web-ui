97d7c9c259aeaf19ef08bd7235cf46c5a5dcfc1a
davidlacho <david.lacho@gmail.com>
2020-03-16 15:11:24 -0700

    FW-1093: Characters mapped and dc:title cleaned

--- FirstVoicesData/src/main/java/ca/firstvoices/document/services/CleanupCharactersService.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/document/services/CleanupCharactersService.java
@@ -2,10 +2,11 @@ package ca.firstvoices.document.services;
 
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.api.DocumentModelList;
 
 import java.util.Map;
 
 public interface CleanupCharactersService {
-    void cleanUnicodeCharacters(CoreSession session, DocumentModel document, Event event);
+        void cleanUnicodeCharacters(CoreSession session, DocumentModel document, String[] types, String propertyName);
+        Map<String, String> mapAndValidateConfusableCharacters(DocumentModelList characters);
 }
