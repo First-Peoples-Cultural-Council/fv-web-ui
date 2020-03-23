1b50147b6f476937eabf2b3188504f0c2a56bc6d
Nolan Van Hell <nolan@fpcc.ca>
2020-03-23 14:12:44 -0700

    FW-1081 Add check for existing issue comment

--- scripts/ci/ReviewerInfoComment.sh
+++ scripts/ci/ReviewerInfoComment.sh
@@ -53,10 +53,16 @@ do
             FOUND=true
         fi
         if [[ $FOUND = true ]]; then
-            echo ${BASH_REMATCH} ": Adding pull request reviewer info to issue."
-            jira comment --noedit --comment="The pull request for this issue was reviewed by https://github.com/$REVIEWER" ${BASH_REMATCH}
+            issue_info=$(jira view ${BASH_REMATCH})
+            ISSUE=${BASH_REMATCH}
+            if [[ ! $(echo $issue_info | grep -o 'comments:[[:space:]].*') =~ The[[:space:]]pull[[:space:]]request[[:space:]]for[[:space:]]this[[:space:]]issue[[:space:]]was[[:space:]]reviewed[[:space:]]by[[:space:]]https:\/\/github\.com\/${REVIEWER} ]]; then
+                echo ${ISSUE} ": Adding pull request reviewer info to issue:" ${REVIEWER}
+#            jira comment --noedit --comment="The pull request for this issue was reviewed by https://github.com/$REVIEWER" ${ISSUE}
+            else
+                echo ${ISSUE} ": Issue already reviewed by" ${REVIEWER}
+            fi
         else
-            echo ${BASH_REMATCH} ": Issue not found on jira. No comment added."
+            echo ${ISSUE} ": Issue not found on jira. No comment added."
         fi
     fi
     echo ''
