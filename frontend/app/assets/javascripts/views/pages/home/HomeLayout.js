3cfe713523d8c16248aaa65c69d09dcdfc4558a7
Chad Oakenfold <web@oakenfold.ca>
2020-05-31 20:38:05 -0700

    Data Analysis
    - Added comments in HomeLayout where data is accessed; created an initial proposal for the output of the data component
    - Looked into the needs of a child component (IntroCardView) and added some notes in there

--- frontend/app/assets/javascripts/views/pages/home/HomeLayout.js
+++ frontend/app/assets/javascripts/views/pages/home/HomeLayout.js
@@ -47,70 +47,8 @@ import HomeData from 'views/pages/home/HomeData'
  * Explore Archive page shows all the families in the archive
  */
 
-const { func, object, string } = PropTypes
 export class HomeLayout extends Component {
-  static propTypes = {
-    // REDUX: reducers/state
-    computeLogin: object.isRequired,
-    computePage: object.isRequired,
-    computeUserStartpage: object.isRequired,
-    properties: object.isRequired,
-    windowPath: string.isRequired,
-    // REDUX: actions/dispatch/func
-    fetchUserStartpage: func.isRequired,
-    pushWindowPath: func.isRequired,
-    queryPage: func.isRequired,
-  }
-
-  constructor(props, context) {
-    super(props, context)
-
-    this.state = {
-      pagePath: '/' + this.props.properties.domain + '/sections/Site/Resources/',
-      dialectsPath: '/' + this.props.properties.domain + '/sections/',
-    }
-    ;['_onNavigateRequest', '_getBlockByArea'].forEach((method) => (this[method] = this[method].bind(this)))
-  }
-
-  async componentDidMount() {
-    await this.props.queryPage(
-      this.state.pagePath,
-      " AND fvpage:url LIKE '/home/'" + '&sortOrder=ASC' + '&sortBy=dc:title'
-    )
-    // Get user start page
-    await this.props.fetchUserStartpage('currentUser', {
-      defaultHome: false,
-    })
-  }
-
-  componentDidUpdate() {
-    /*
-    Redirect user to their start page if they:
-    - are members of a single dialect
-    - have one defined
-    */
-
-    // If user is accessing /home directly, do not redirect.
-    if (this.props.windowPath.indexOf('/home') !== -1) {
-      return
-    }
-    const _computeUserStartpage = ProviderHelpers.getEntry(this.props.computeUserStartpage, 'currentUser')
-    const startPage = selectn('response.value', _computeUserStartpage)
-    if (startPage) {
-      window.location = startPage
-    }
-  }
-
-  _onNavigateRequest(path) {
-    this.props.pushWindowPath(path)
-  }
-
-  _getBlockByArea(page, area) {
-    return (selectn('fvpage:blocks', page) || []).filter((block) => {
-      return block.area == area
-    })
-  }
-
+  // ----------------------------------------
   render() {
     let bgAlign = 'center'
 
@@ -197,138 +135,213 @@ export class HomeLayout extends Component {
     }
 
     return (
-      <PromiseWrapper renderOnError computeEntities={computeEntities}>
-        <HomeData>
-          {(homeDataOutput) => {
-            // eslint-disable-next-line
-            console.log('HomeData Liminal', homeDataOutput)
-            return <h1>HomeData Liminal</h1>
-          }}
-        </HomeData>
-        <div className="row" style={homePageStyle}>
-          <div style={{ position: 'relative', height: '650px' }}>
-            <div className={classNames('col-xs-12')} style={{ height: '100%' }}>
-              <div className="home-intro-block">
-                <h1
-                  className="display"
-                  style={{
-                    backgroundColor: 'rgba(180, 0, 0, 0.65)',
-                    fontWeight: 500,
-                  }}
-                >
-                  {this.props.intl.searchAndReplace(selectn('fvpage:blocks[0].title', page), {})}
-                </h1>
-                <div className={classNames('home-intro-p-cont', 'body')}>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: this.props.intl.searchAndReplace(selectn('fvpage:blocks[0].text', page), {}),
-                    }}
-                  />
+      <HomeData>
+        {(homeDataOutput) => {
+          /*
+Looks like the component can render with the following data:
+{
+  sections: [{
+    // IntroCardView
+    file: {
+      data: '',
+    },
+    title: '', // IntroCardView, & General
+    summary: '', // IntroCardView
+    text: '', // General
+  }],
+  properties: '', // TextHeader
+  computeEntities: '', // PromiseWrapper
+  primary1Color: '', // IntroCardView
+  primary2Color: '', // IntroCardView
+  accessButtons: [{
+    url: '',
+    text: ''
+  }],
+}
+*/
+          // eslint-disable-next-line
+          console.log('HomeData Liminal', homeDataOutput)
+
+          return (
+            // DATA: computeEntities
+            <PromiseWrapper renderOnError computeEntities={computeEntities}>
+              <div className="row" style={homePageStyle}>
+                <div style={{ position: 'relative', height: '650px' }}>
+                  <div className={classNames('col-xs-12')} style={{ height: '100%' }}>
+                    <div className="home-intro-block">
+                      <h1
+                        className="display"
+                        style={{
+                          backgroundColor: 'rgba(180, 0, 0, 0.65)',
+                          fontWeight: 500,
+                        }}
+                      >
+                        {/* DATA:
+                        blocks[0].title
+                        blocks[0].text
+                      */}
+                        {this.props.intl.searchAndReplace(selectn('fvpage:blocks[0].title', page), {})}
+                      </h1>
+                      <div className={classNames('home-intro-p-cont', 'body')}>
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: this.props.intl.searchAndReplace(selectn('fvpage:blocks[0].text', page), {}),
+                          }}
+                        />
+                      </div>
+                      <div>{accessButtons}</div>
+                    </div>
+                  </div>
                 </div>
-                <div>{accessButtons}</div>
               </div>
-            </div>
-          </div>
-        </div>
 
-        <div className={classNames('row')} style={{ margin: '25px 0' }}>
-          <div>
-            {this._getBlockByArea(page, 1).map((block, i) => {
-              return (
-                <div key={i} className={classNames('col-xs-12')}>
-                  <div className="body">
-                    <h2 style={{ fontWeight: 500 }}>{this.props.intl.searchAndReplace(selectn('title', block))}</h2>
-                    <p dangerouslySetInnerHTML={{ __html: selectn('text', block) }} />
+              <div className={classNames('row')} style={{ margin: '25px 0' }}>
+                <div>
+                  {/* DATA:
+                  blocks[1].title
+                  blocks[1].text
+                */}
+                  {this._getBlockByArea(page, 1).map((block, i) => {
+                    return (
+                      <div key={i} className={classNames('col-xs-12')}>
+                        <div className="body">
+                          <h2 style={{ fontWeight: 500 }}>
+                            {this.props.intl.searchAndReplace(selectn('title', block))}
+                          </h2>
+                          <p dangerouslySetInnerHTML={{ __html: selectn('text', block) }} />
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+
+              <div className={classNames('row')} style={{ margin: '25px 0' }}>
+                {/* DATA:
+                  blocks[2].file.data
+                  blocks[2].title
+                  blocks[2].summary
+                  properties
+                */}
+                {this._getBlockByArea(page, 2).length > 0 ? (
+                  <div className={classNames('col-xs-12')} style={{ marginBottom: '15px' }}>
+                    <TextHeader
+                      title={this.props.intl.translate({
+                        key: ['views', 'pages', 'home', 'tools_and_resources'],
+                        default: 'TOOLS &amp; RESOURCES',
+                        case: 'words',
+                      })}
+                      properties={this.props.properties}
+                    />
                   </div>
+                ) : null}
+
+                <div>
+                  {this._getBlockByArea(page, 2).map((block, i) => {
+                    return (
+                      <div key={i} className={classNames('col-xs-12', 'col-md-3')}>
+                        <IntroCardView block={block} primary1Color={primary1Color} primary2Color={primary2Color} />
+                      </div>
+                    )
+                  })}
                 </div>
-              )
-            })}
-          </div>
-        </div>
+              </div>
 
-        <div className={classNames('row')} style={{ margin: '25px 0' }}>
-          {this._getBlockByArea(page, 2).length > 0 ? (
-            <div className={classNames('col-xs-12')} style={{ marginBottom: '15px' }}>
-              <TextHeader
-                title={this.props.intl.translate({
-                  key: ['views', 'pages', 'home', 'tools_and_resources'],
-                  default: 'TOOLS &amp; RESOURCES',
-                  case: 'words',
-                })}
-                properties={this.props.properties}
-              />
-            </div>
-          ) : null}
-
-          <div>
-            {this._getBlockByArea(page, 2).map((block, i) => {
-              return (
-                <div key={i} className={classNames('col-xs-12', 'col-md-3')}>
-                  <IntroCardView block={block} primary1Color={primary1Color} primary2Color={primary2Color} />
-                </div>
-              )
-            })}
-          </div>
-        </div>
-
-        <div className={classNames('row')} style={{ margin: '25px 0' }}>
-          {this._getBlockByArea(page, 3).length > 0 ? (
-            <div className={classNames('col-xs-12')} style={{ marginBottom: '15px' }}>
-              <TextHeader
-                title={this.props.intl.translate({
-                  key: ['views', 'pages', 'home', 'news_and_updates'],
-                  default: 'NEWS &amp; UPDATES',
-                  case: 'words',
-                })}
-                properties={this.props.properties}
-              />
-            </div>
-          ) : null}
-
-          <div>
-            {this._getBlockByArea(page, 3).map((block, i) => {
-              return (
-                <div key={i} className={classNames('col-xs-12', 'col-md-3')}>
-                  <IntroCardView block={block} primary1Color={primary1Color} primary2Color={primary2Color} />
-                </div>
-              )
-            })}
-          </div>
-        </div>
-
-        <div className={classNames('row')} style={{ margin: '25px 0' }}>
-          {this._getBlockByArea(page, 4).length > 0 ? (
-            <div className={classNames('col-xs-12')} style={{ marginBottom: '15px' }}>
-              <TextHeader
-                title={this.props.intl.translate({
-                  key: ['views', 'pages', 'home', 'compatibility'],
-                  default: 'COMBATIBILITY',
-                  case: 'words',
-                })}
-                properties={this.props.properties}
-              />
-            </div>
-          ) : null}
-
-          <div>
-            {this._getBlockByArea(page, 4).map((block, i) => {
-              return (
-                <div key={i} className={classNames('col-xs-12')}>
-                  <div className="body">
-                    <h2 style={{ fontWeight: 500 }}>{this.props.intl.searchAndReplace(selectn('title', block))}</h2>
-                    <p dangerouslySetInnerHTML={{ __html: this.props.intl.searchAndReplace(selectn('text', block)) }} />
+              <div className={classNames('row')} style={{ margin: '25px 0' }}>
+                {/* DATA:
+                  blocks[3].file.data
+                  blocks[3].title
+                  blocks[3].summary
+                  properties
+                */}
+                {this._getBlockByArea(page, 3).length > 0 ? (
+                  <div className={classNames('col-xs-12')} style={{ marginBottom: '15px' }}>
+                    <TextHeader
+                      title={this.props.intl.translate({
+                        key: ['views', 'pages', 'home', 'news_and_updates'],
+                        default: 'NEWS &amp; UPDATES',
+                        case: 'words',
+                      })}
+                      properties={this.props.properties}
+                    />
                   </div>
+                ) : null}
+
+                <div>
+                  {this._getBlockByArea(page, 3).map((block, i) => {
+                    return (
+                      <div key={i} className={classNames('col-xs-12', 'col-md-3')}>
+                        <IntroCardView block={block} primary1Color={primary1Color} primary2Color={primary2Color} />
+                      </div>
+                    )
+                  })}
                 </div>
-              )
-            })}
-          </div>
-        </div>
-      </PromiseWrapper>
+              </div>
+
+              <div className={classNames('row')} style={{ margin: '25px 0' }}>
+                {/* DATA:
+                  blocks[4].title
+                  blocks[4].text
+                  properties
+                */}
+                {this._getBlockByArea(page, 4).length > 0 ? (
+                  <div className={classNames('col-xs-12')} style={{ marginBottom: '15px' }}>
+                    <TextHeader
+                      title={this.props.intl.translate({
+                        key: ['views', 'pages', 'home', 'compatibility'],
+                        default: 'COMBATIBILITY',
+                        case: 'words',
+                      })}
+                      properties={this.props.properties}
+                    />
+                  </div>
+                ) : null}
+
+                <div>
+                  {this._getBlockByArea(page, 4).map((block, i) => {
+                    return (
+                      <div key={i} className={classNames('col-xs-12')}>
+                        <div className="body">
+                          <h2 style={{ fontWeight: 500 }}>
+                            {this.props.intl.searchAndReplace(selectn('title', block))}
+                          </h2>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html: this.props.intl.searchAndReplace(selectn('text', block)),
+                            }}
+                          />
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+            </PromiseWrapper>
+          )
+        }}
+      </HomeData>
     )
   }
 }
 
+// PROPTYPES
+// ----------------------------------------
+const { func, object, string } = PropTypes
+HomeLayout.propTypes = {
+  // REDUX: reducers/state
+  computeLogin: object.isRequired,
+  computePage: object.isRequired,
+  computeUserStartpage: object.isRequired,
+  properties: object.isRequired,
+  windowPath: string.isRequired,
+  // REDUX: actions/dispatch/func
+  fetchUserStartpage: func.isRequired,
+  pushWindowPath: func.isRequired,
+  queryPage: func.isRequired,
+}
+
 // REDUX: reducers/state
+// ----------------------------------------
 const mapStateToProps = (state /*, ownProps*/) => {
   const { fvPage, fvUser, navigation, nuxeo, windowPath, locale } = state
 
