b8c981da7f48caaeb94941a5e2d3c86b56d092f5
Chad Oakenfold <web@oakenfold.ca>
2020-06-03 15:27:25 -0700

    Added notes on the usePrevious custom hook

--- frontend/app/assets/javascripts/common/DataSource/usePrevious.js
+++ frontend/app/assets/javascripts/common/DataSource/usePrevious.js
@@ -1,10 +1,12 @@
+// NOTE: How to get the previous props or state?
+// https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
 import { useEffect, useRef } from 'react'
 
 function usePrevious(value) {
   const ref = useRef()
   useEffect(() => {
     ref.current = value
-  })
+  }, [value])
   return ref.current
 }
 
