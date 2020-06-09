7d867960ee96b29f5768653de9e487bb93b6dcc2
Daniel Yona <daniel@fpcc.ca>
2020-06-09 11:38:30 -0700

    Cypress changes

--- frontend/scripts/start_e2e_tests.sh
+++ frontend/scripts/start_e2e_tests.sh
@@ -7,10 +7,12 @@ ENDCOLOR="\e[0m"
 # Install wait-on to wait for site to be available before running tests
 npm install -g wait-on
 
+echo "Cypress Base URL is set to {$CYPRESS_BASE_URL}"
+
 # Wait for last language to be setup and accessible
-wait-on $CYPRESS_baseUrl/nuxeo/api/v1/path/FV/sections/Data/Test/Test/TestLanguageEight && \
+wait-on $CYPRESS_BASE_URL/nuxeo/api/v1/path/FV/sections/Data/Test/Test/TestLanguageEight && \
     npm ci && \
     npm run cy:trashCopy && \ 
     npm run cy:copy && \
-    cypress run --browser chrome --headless #\
-    # --tag "local"  --record true 
+    cypress run --browser chrome --headless \
+    --tag "Jenkins"  --record true
