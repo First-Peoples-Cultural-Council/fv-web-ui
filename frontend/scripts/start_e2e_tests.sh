7f047099be4ba6c8cb3b0362085ce4f3a209fef4
Daniel Yona <daniel@fpcc.ca>
2020-06-09 12:46:31 -0700

    Update base url

--- frontend/scripts/start_e2e_tests.sh
+++ frontend/scripts/start_e2e_tests.sh
@@ -9,10 +9,16 @@ npm install -g wait-on
 
 echo "Cypress Base URL is set to {$CYPRESS_BASE_URL}"
 
+echo "Cat Cypress.json"
+
+echo "***"
+cat /e2e/cypress/cypress.json
+echo "***"
+
 # Wait for last language to be setup and accessible
 wait-on $CYPRESS_BASE_URL/nuxeo/api/v1/path/FV/sections/Data/Test/Test/TestLanguageEight && \
-    npm ci && \
-    npm run cy:trashCopy && \ 
-    npm run cy:copy && \
-    cypress run --browser chrome --headless \
+    npm ci --quiet && \
+    npm run --silent cy:trashCopy && \
+    npm run --silent cy:copy && \
+    cypress run --silent --browser chrome --headless \
     --tag "Jenkins"  --record true
