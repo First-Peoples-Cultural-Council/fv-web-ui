9d903ee4e31fa9aa3a6d707b9cd8dc23d97a14c7
davidlacho <david.lacho@gmail.com>
2020-06-24 14:13:04 -0700

    FW-1549: Sonar Fixes

--- FirstVoicesOperations/src/main/java/ca/firstvoices/visibility/operations/UpdateVisibilityOperation.java
+++ FirstVoicesOperations/src/main/java/ca/firstvoices/visibility/operations/UpdateVisibilityOperation.java
@@ -25,11 +25,9 @@ public class UpdateVisibilityOperation {
   @Param(name = "visibility", values = {TEAM, MEMBERS, PUBLIC})
   private String visibility;
 
-  private UpdateVisibilityService service;
-
   @OperationMethod
   public DocumentModel run(DocumentModel doc) {
-    service = Framework.getService(UpdateVisibilityService.class);
+    UpdateVisibilityService service = Framework.getService(UpdateVisibilityService.class);
     return service.updateVisibility(doc, visibility);
   }
 
