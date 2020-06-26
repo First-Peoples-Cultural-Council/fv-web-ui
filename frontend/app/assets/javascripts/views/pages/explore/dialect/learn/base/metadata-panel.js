f90a11c987f92dfd19edbc1855a284ef1fe8d05a
elysse-fpcc <elysse@fpcc.ca>
2020-06-26 15:12:07 -0700

    FW-296 Modified metadata panel wording to include dialect name

--- frontend/app/assets/javascripts/views/pages/explore/dialect/learn/base/metadata-panel.js
+++ frontend/app/assets/javascripts/views/pages/explore/dialect/learn/base/metadata-panel.js
@@ -53,7 +53,7 @@ export class MetadataPanel extends Component {
   }
 
   render() {
-    const { computeEntity } = this.props
+    const {computeEntity, routeParams} = this.props
 
     const metadata = []
 
@@ -121,10 +121,12 @@ export class MetadataPanel extends Component {
     /**
      * Status
      */
+    const dialectName = routeParams.dialect_name
+
     const mapDocumentStateToVisibility = {
-      New: "Team Only",
-      Disabled: "Team Only",
-      Enabled: "Members Only",
+      New: `${dialectName} Team Only`,
+      Disabled: `${dialectName} Team Only`,
+      Enabled: `${dialectName} Members Only`,
       Published: "Public"
     }
 
@@ -192,11 +194,12 @@ export class MetadataPanel extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const { locale } = state
-  const { intlService } = locale
+  const {locale, navigation} = state
+  const {intlService} = locale
 
   return {
     intl: intlService,
+    routeParams: navigation.route.routeParams,
   }
 }
 
