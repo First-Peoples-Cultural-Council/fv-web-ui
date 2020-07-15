d5ef2c28219f0b5ed0f8134058285bea8d35242e
Chad Oakenfold <web@oakenfold.ca>
2020-07-14 21:11:30 -0700

    DashboardDetailListItemPresentation
    - zebra striping
    - selected styling

--- frontend/app/assets/javascripts/components/DashboardDetail/DashboardDetailListItem/DashboardDetailListItemPresentation.js
+++ frontend/app/assets/javascripts/components/DashboardDetail/DashboardDetailListItem/DashboardDetailListItemPresentation.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
+import useTheme from 'DataSource/useTheme'
+import selectn from 'selectn'
 import '!style-loader!css-loader!./DashboardDetailListItem.css'
+
 /**
  * @summary DashboardDetailListItemPresentation
  * @version 1.0.1
@@ -9,46 +12,71 @@ import '!style-loader!css-loader!./DashboardDetailListItem.css'
  *
  * @param {object} props
  * @param {string} props.component String to set the html tags used
- * @param {node} props.icon JSX for the icon
- * @param {string} props.title
- * @param {string} props.requestedBy
  * @param {string} props.date
+ * @param {node} props.icon JSX for the icon
+ * @param {string} props.initiator
+ * @param {boolean} props.isActive
+ * @param {function} props.onClick
+ * @param {string} props.title
+ * @param {number} props.variant
  *
  * @returns {node} jsx markup
  */
-function DashboardDetailListItemPresentation({ component, icon, title, requestedBy, date }) {
-  const Component = component
+
+const EVEN = 0
+const ODD = 1
+
+function DashboardDetailListItemPresentation({ component, date, icon, initiator, isActive, onClick, title, variant }) {
+  const { theme } = useTheme()
+  const themeTable = selectn('components.Table', theme) || {}
+  const { row, rowAlternate } = themeTable
+
+  const ItemStyle = []
+  ItemStyle[EVEN] = row
+  ItemStyle[ODD] = rowAlternate
+
+  const Tag = component
   return (
-    <Component
-      className={`DashboardDetailListItem ${
+    <Tag
+      className={`DashboardDetailListItem  DashboardDetailListItem--${component} ${
         icon ? 'DashboardDetailListItem--hasIcon' : ''
-      } DashboardDetailListItem--${component}`}
+      } ${isActive ? 'DashboardDetailListItem--isActive' : ''}`}
+      onClick={onClick}
+      style={ItemStyle[variant]}
     >
       {icon && <div className="DashboardDetailListItem__icon">{icon}</div>}
       <div className="DashboardDetailListItem__main">
-        <Typography variant="h6" component="h3">
+        <Typography variant="body1" component="h3">
           {title}
         </Typography>
-        <div>Requested by {requestedBy}</div>
-        <div>{date}</div>
+        <Typography variant="body2" component="div">
+          Requested by {initiator}
+        </Typography>
+        <Typography variant="caption" component="div">
+          {date}
+        </Typography>
       </div>
-    </Component>
+    </Tag>
   )
 }
 // PROPTYPES
-const { node, oneOf, string } = PropTypes
+const { func, node, oneOf, string, bool } = PropTypes
 DashboardDetailListItemPresentation.propTypes = {
-  icon: node,
-  title: string,
-  requestedBy: string,
-  date: string,
   component: oneOf(['div', 'li']),
+  date: string,
+  icon: node,
+  initiator: string,
+  isActive: bool,
+  onClick: func,
+  title: string,
+  variant: oneOf([EVEN, ODD]),
 }
 DashboardDetailListItemPresentation.defaultProps = {
-  title: '',
-  requestedBy: '',
-  date: '',
   component: 'li',
+  date: '',
+  initiator: '',
+  onClick: () => {},
+  title: '',
 }
 
 export default DashboardDetailListItemPresentation
