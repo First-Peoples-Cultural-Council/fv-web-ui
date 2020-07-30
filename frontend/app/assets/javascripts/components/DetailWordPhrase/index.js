4baa24d64d831f6fdd49501d8c5a8179bbb8ce66
Chad Oakenfold <web@oakenfold.ca>
2020-07-30 10:56:18 -0700

    Words Core Component
    Extracted a component that renders data on Words Detail. Planning to reuse it on Dashboard Detail.

---
+++ frontend/app/assets/javascripts/components/DetailWordPhrase/index.js
@@ -0,0 +1,9 @@
+import DetailWordPhraseContainer from 'components/DetailWordPhrase/DetailWordPhraseContainer'
+import DetailWordPhrasePresentation from 'components/DetailWordPhrase/DetailWordPhrasePresentation'
+import DetailWordPhraseData from 'components/DetailWordPhrase/DetailWordPhraseData'
+
+export default {
+  Container: DetailWordPhraseContainer,
+  Presentation: DetailWordPhrasePresentation,
+  Data: DetailWordPhraseData,
+}
