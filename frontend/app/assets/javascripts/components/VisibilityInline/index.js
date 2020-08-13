fb8718d93e79aa6d751026469a5793de85a6a65b
elysse-fpcc <elysse@fpcc.ca>
2020-08-13 11:50:56 -0700

    FW-1601 Removed outdated files brought in by merge from master

--- frontend/app/assets/javascripts/components/VisibilityInline/index.js
+++
@@ -1,9 +0,0 @@
-import VisibilityInlineContainer from 'components/VisibilityInline/VisibilityInlineContainer'
-import VisibilityInlinePresentation from 'components/VisibilityInline/VisibilityInlinePresentation'
-import VisibilityInlineData from 'components/VisibilityInline/VisibilityInlineData'
-
-export default {
-  Container: VisibilityInlineContainer,
-  Presentation: VisibilityInlinePresentation,
-  Data: VisibilityInlineData,
-}
