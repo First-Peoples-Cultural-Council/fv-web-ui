407ccd7ba9176a00c2c3ae883df09f43c6fb8317
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-08-18 16:23:36 -0700

    Add Word component

---
+++ frontend/app/assets/javascripts/components/Word/WordStyles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const WordStyles = makeStyles({
+  base: {
+    border: 'solid',
+  },
+})
---
+++ frontend/app/assets/javascripts/components/Word/index.js
@@ -0,0 +1,9 @@
+import WordContainer from 'components/Word/WordContainer'
+import WordPresentation from 'components/Word/WordPresentation'
+import WordData from 'components/Word/WordData'
+
+export default {
+  Container: WordContainer,
+  Presentation: WordPresentation,
+  Data: WordData,
+}
