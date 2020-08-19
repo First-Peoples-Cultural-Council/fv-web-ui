986de56aef0a49a5e9f890f7c8c80ce553cfc034
Daniel Yona <daniel@fpcc.ca>
2020-08-19 15:46:27 -0700

    Final cleanup for simple task endpoints

--- FirstVoicesData/src/main/java/ca/firstvoices/models/SimpleCoreEntity.java
+++ FirstVoicesData/src/main/java/ca/firstvoices/models/SimpleCoreEntity.java
@@ -1,28 +1,52 @@
 package ca.firstvoices.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.io.Serializable;
+import org.nuxeo.ecm.core.api.DocumentModel;
 
+/**
+ * Simple Core Entity object is intended to represent a simplified view of any type in FirstVoices
+ * It is mostly to be used by an enricher or marshaller to represent a sub-document
+ * It should reference fields that are relevant to all types
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SimpleCoreEntity {
+@JsonPropertyOrder({"uid", "title", "type", "isNew" })
+public class SimpleCoreEntity implements Serializable {
 
-  private String title;
+  @JsonProperty("uid")
+  private final String uid;
 
-  private boolean isNew = false;
+  @JsonProperty("title")
+  private final String title;
+
+  @JsonProperty("type")
+  private final String type;
+
+  @JsonProperty("isNew")
+  private final Boolean isNew;
+
+  public SimpleCoreEntity(DocumentModel doc) {
+    this.title = String.valueOf(doc.getPropertyValue("dc:title"));
+    this.type = doc.getType();
+    this.isNew = ("New".equals(doc.getCurrentLifeCycleState()));
+    this.uid = doc.getId();
+  }
 
   public String getTitle() {
     return title;
   }
 
-  public void setTitle(String title) {
-    this.title = title;
+  public String getType() {
+    return type;
   }
 
-  public boolean isNew() {
+  public Boolean getIsNew() {
     return isNew;
   }
 
-  public void setNew(boolean isNew) {
-    this.isNew = isNew;
+  public String getUid() {
+    return uid;
   }
-
 }
