9cbab55577f3a73e1602c70c0fe6f0eb8dc2437a
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-08-31 16:47:51 -0700

    Remove unused files

--- frontend/app/assets/javascripts/components/StoryCover/index.js
+++ frontend/app/assets/javascripts/components/StoryCover/index.js
@@ -1,9 +1,5 @@
-import StoryCoverContainer from 'components/StoryCover/StoryCoverContainer'
 import StoryCoverPresentation from 'components/StoryCover/StoryCoverPresentation'
-import StoryCoverData from 'components/StoryCover/StoryCoverData'
 
 export default {
-  Container: StoryCoverContainer,
   Presentation: StoryCoverPresentation,
-  Data: StoryCoverData,
 }
