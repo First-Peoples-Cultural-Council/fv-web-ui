241fb1770afde5e639a317d4d2252062a2c982fc
Chad Oakenfold <web@oakenfold.ca>
2020-09-09 08:58:23 -0700

    Dropped unnecessary files

--- frontend/app/assets/javascripts/components/ActivityStream/index.js
+++
@@ -1,9 +0,0 @@
-import ActivityStreamContainer from 'components/ActivityStream/ActivityStreamContainer'
-import ActivityStreamPresentation from 'components/ActivityStream/ActivityStreamPresentation'
-import ActivityStreamData from 'components/ActivityStream/ActivityStreamData'
-
-export default {
-  Container: ActivityStreamContainer,
-  Presentation: ActivityStreamPresentation,
-  Data: ActivityStreamData,
-}
