393645213a815345f7c0b7be2b39604f3fbd8d51
elysse-fpcc <elysse@fpcc.ca>
2020-09-09 17:23:05 -0700

    FW-1743 Finished presentation layer for songs and stories

--- frontend/app/assets/javascripts/components/DetailSongStory/DetailSongStoryPresentation.js
+++ frontend/app/assets/javascripts/components/DetailSongStory/DetailSongStoryPresentation.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Preview from 'views/components/Editor/Preview'
-import FVLabel from 'views/components/FVLabel/index'
 import {SongStoryCoverStyles} from '../SongStoryCover/SongStoryCoverStyles'
 import '!style-loader!css-loader!./DetailSongStory.css'
 import Grid from '@material-ui/core/Grid'
@@ -11,7 +10,7 @@ import ActionLaunch from '@material-ui/icons/Launch'
 import DOMPurify from 'dompurify'
 import MediaPanels from 'components/MediaPanels'
 import FVButton from 'views/components/FVButton'
-import FVTab from 'views/components/FVTab'
+import Divider from '@material-ui/core/Divider'
 
 /**
  * @summary DetailSongStoryPresentation
@@ -34,128 +33,116 @@ function DetailSongStoryPresentation({
   // Audio
   const audioMapped = audio.map((audioDoc) => {
     return <Preview minimal key={audioDoc.uid} expandedValue={audioDoc.object}
-                    type="FVAudio"/>
+      type="FVAudio"/>
   })
   const audioElements = audioMapped && audioMapped.length !== 0 ? audioMapped
-      : null
+    : null
 
   const mediaPanels =
-      videos.length > 0 || pictures.length > 0 ? (
-          <Grid key={'media-' + book.uid} item xs={2}>
-            <div className={classes.media}>
-              <MediaPanels.Presentation pictures={pictures} videos={videos}/>
-            </div>
-          </Grid>
-      ) : null
-
-  // Main component
-  return (
-      <Paper elevation={3} className={classes.cover}>
-        <Grid key={book.uid} container className={classes.gridRoot} spacing={2}>
-          <Grid container justify="center" spacing={2}>
-            {mediaPanels}
-            <Grid key={'text-' + book.uid} item xs={5}>
-              <header className={classes.header}>
-                <div className="DetailSongStory__heading">
-                  <Typography variant="h4" component="h3">
-                    <div>{book.title}</div>
-                  </Typography>
-                  <Typography variant="h5" component="h4">
-                    <div>{book.titleTranslation}</div>
-                  </Typography>
-                  <div className="subheader">
-                    {book.authors.map(function renderAuthors(author, i) {
-                      return (
-                          <span className="label label-default" key={i}>
-                        {author}
-                      </span>
-                      )
-                    })}
-                  </div>
-                </div>
-
-                <div
-                    className="DetailSongStory__audioPlayer">{audioElements}</div>
-              </header>
-              <div className={classes.introduction}>
-                {_getIntroduction(book.introduction,
-                    book.introductionTranslation)}
-
-              </div>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <div className="text-right">
-            <FVButton variant="contained" style={{marginRight: '10px'}}
-                      color="primary" onClick={openBookAction}>
-              <ActionLaunch/>
-              {'View full '} {book.type}
-            </FVButton>
+      // If both videos and pictures are present, only include pictures
+      pictures.length > 0 ? (
+        <Grid key={'media-' + book.uid} item xs={2}>
+          <div className={classes.media}>
+            <MediaPanels.Presentation pictures={pictures} videos={[]}/>
           </div>
         </Grid>
-      </Paper>
+      ) : videos.length > 0 ? (
+        <Grid key={'media-' + book.uid} item xs={2}>
+          <div className={classes.media}>
+            <MediaPanels.Presentation pictures={[]} videos={videos}/>
+          </div>
+        </Grid>
+      ) : null
+  // Main component
+  return (
+    <Paper elevation={3} className={classes.cover}>
+      <Grid key={book.uid} container className={classes.gridRoot} spacing={2}>
+        <Grid container justify="center" spacing={2}>
+          {mediaPanels}
+          <Grid key={'text-' + book.uid} item xs={5}>
+            <div className="DetailSongStory__header">
+              <Typography variant="h4" component="h3">
+                <div>{book.title}</div>
+              </Typography>
+              <Typography variant="h5" component="h4">
+                <div>{book.titleTranslation}</div>
+              </Typography>
+              <div className="subheader">
+                {book.authors.map(function renderAuthors(author, i) {
+                  return (
+                    <span className="label label-default" key={i}>
+                      {author}
+                    </span>
+                  )
+                })}
+              </div>
+            </div>
+            <div
+              className="DetailSongStory__audioPlayer">{audioElements}</div>
+            <div className={classes.introduction}>
+              {_getIntroduction(book.introduction,
+                book.introductionTranslation)}
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <div className="text-right">
+          <FVButton variant="contained" style={{marginRight: '10px'}}
+            color="primary" onClick={openBookAction}>
+            <ActionLaunch/>
+            {'View full '} {book.type}
+          </FVButton>
+        </div>
+      </Grid>
+    </Paper>
   )
 }
 
 function _getIntroduction(introduction, introductionTranslation) {
-  const classes = SongStoryCoverStyles()
-  const [tabValue, setTabValue] = useState(0)
   const introductionDiv = (
-      <div className={classes.introductionContent}>
-        <div dangerouslySetInnerHTML={{
-          __html: DOMPurify.sanitize(introduction.content),
-        }}/>
-      </div>
+    <div className="DetailSongStory__introduction">
+      <Typography variant="h5" component="h4">
+        <div>Introduction</div>
+      </Typography>
+      <div dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(introduction.content),
+      }}/>
+    </div>
   )
+
+  const translationDiv = introductionTranslation.content ? (
+    <div className="DetailSongStory__translation">
+      <Typography variant="h5" component="h4">
+        <div>Translation</div>
+      </Typography>
+      <div dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(introductionTranslation.content),
+      }}/>
+    </div>
+  ) : null
+
+  const displayDivider = introductionTranslation.content ? (
+    <Divider variant="middle"/>
+  ) : null
 
   if (!introductionTranslation.content) {
     if (!introduction.content) {
       return null
     }
-
-    return (
-        <div className={classes.introductionTranslation}>
-          <div>
-            <h1 className={classes.introductionTitle}>
-              <FVLabel transKey="introduction" defaultStr="Introduction"
-                       transform="first"/>
-            </h1>
-          </div>
-          {introductionDiv}
-        </div>
-    )
   }
 
   return (
-      <div>
-        <FVTab
-            tabItems={[{label: introduction.label},
-              {label: introductionTranslation.label}]}
-            tabsValue={tabValue}
-            tabsOnChange={(e, value) => setTabValue(value)}
-        />
-
-        {tabValue === 0 && (
-            <Typography variant="h5" component="div" style={{padding: 8 * 3}}>
-              {introductionDiv}
-            </Typography>
-        )}
-        {tabValue === 1 && (
-            <Typography variant="h5" component="div" style={{padding: 8 * 3}}>
-              <div className={classes.introductionContent}>
-                <div dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(introductionTranslation.content),
-                }}/>
-              </div>
-            </Typography>
-        )}
-      </div>
+    <Paper>
+      {introductionDiv}
+      {displayDivider}
+      {translationDiv}
+    </Paper>
   )
 }
 
 // PROPTYPES
-const {array, node, func, string, object, number} = PropTypes
+const {array, func, string, object, number} = PropTypes
 DetailSongStoryPresentation.propTypes = {
   book: object.isRequired,
   defaultLanguage: string,
@@ -167,6 +154,5 @@ DetailSongStoryPresentation.propTypes = {
   pictures: array.isRequired,
   videos: array.isRequired,
 }
-
 
 export default DetailSongStoryPresentation
