eeb379cfef6917d1a381b354a3bf4b75a6fa17a9
Chad Oakenfold <web@oakenfold.ca>
2020-09-23 09:30:22 -0700

    FW-1855

--- frontend/app/assets/javascripts/common/StringHelpers.js
+++ frontend/app/assets/javascripts/common/StringHelpers.js
@@ -156,14 +156,29 @@ export default {
 
     return params
   },
-  visibilityCategories: ({ visibility, dialectName = 'Community' }) => {
-    switch (visibility) {
+  visibilityText: ({ visibility = '', dialectName = 'Community' }) => {
+    switch (visibility.toLowerCase()) {
       case 'public':
         return 'Public'
+
+      case 'published':
+        return 'Public'
+
       case 'team':
         return 'Language Team'
+
+      case 'new':
+        return 'Language Team'
+
+      case 'disabled':
+        return 'Language Team'
+
+      case 'enabled':
+        return `${dialectName} Members`
+
       case 'members':
         return `${dialectName} Members`
+
       default:
         return visibility
     }
