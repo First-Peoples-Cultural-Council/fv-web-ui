4f57857a8baa89d4f64a96a5e30d9848057cd1fe
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-11-16 16:11:08 -0800

    Delete NavigationBar - replaced by index.html

--- frontend/app/components/NavigationBar/index.js
+++
@@ -1,9 +0,0 @@
-import NavigationBarContainer from 'components/NavigationBar/NavigationBarContainer'
-import NavigationBarPresentation from 'components/NavigationBar/NavigationBarPresentation'
-import NavigationBarData from 'components/NavigationBar/NavigationBarData'
-
-export default {
-  Container: NavigationBarContainer,
-  Presentation: NavigationBarPresentation,
-  Data: NavigationBarData,
-}
