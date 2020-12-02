6aa0cc947e18e62b567ed13a5f4bfee15f0ba293
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-12-02 11:38:20 -0800

    Css adjusted and tooltip added

--- frontend/app/components/DialectTile.js
+++ frontend/app/components/DialectTile.js
@@ -7,6 +7,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import LockIcon from '@material-ui/icons/Lock'
+import Tooltip from '@material-ui/core/Tooltip'
 
 import NavigationHelpers from 'common/NavigationHelpers'
 
@@ -18,6 +19,7 @@ const styles = () => ({
     margin: 10,
     textDecoration: 'none',
     border: 1,
+    position: 'relative',
   },
   details: {
     display: 'flex',
@@ -32,8 +34,24 @@ const styles = () => ({
   },
   privateIcon: {
     color: '#D8AF06',
+    position: 'absolute',
+    right: '10px',
+    top: '10px',
   },
 })
+
+const CustomTooltip = withStyles({
+  tooltip: {
+    backgroundColor: '#F7EAA3',
+    borderColor: '#C9BB70',
+    color: 'inherit',
+    fontSize: '1.5rem',
+    margin: '0',
+    border: '1px solid #C9BB70',
+    padding: '10px',
+    textAlign: 'center',
+  },
+})(Tooltip)
 
 function DialectTile({
   classes,
@@ -71,20 +89,29 @@ function DialectTile({
     )
   }
   return (
-    <Card className={`${classes.card} Dialect`}>
-      <CardMedia className={`${classes.cover} DialectCover`} image={dialectCoverImage} title={dialectTitle} />
-
-      <div className={classes.details}>
-        <LockIcon className={classes.privateIcon} />
-        <CardContent className={classes.content}>
-          <span className="DialectTitle fontBCSans">
-            {dialectTitle}
-            {actionIcon}
-          </span>
-          <div className="DialectDescription">{dialectDescription}</div>
-        </CardContent>
-      </div>
-    </Card>
+    <CustomTooltip title="Private site: Please register to access" placement="top">
+      <a
+        href="/register"
+        onClick={(e) => {
+          e.preventDefault()
+          NavigationHelpers.navigate('/register', pushWindowPath, false)
+        }}
+      >
+        <Card className={`${classes.card} DialectPrivate`}>
+          <CardMedia className={`${classes.cover} DialectCover`} image={dialectCoverImage} title={dialectTitle} />
+          <LockIcon className={classes.privateIcon} />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <span className="DialectTitle fontBCSans">
+                {dialectTitle}
+                {actionIcon}
+              </span>
+              <div className="DialectDescription">{dialectDescription}</div>
+            </CardContent>
+          </div>
+        </Card>
+      </a>
+    </CustomTooltip>
   )
 }
 const { bool, object, string, func } = PropTypes
