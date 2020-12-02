ec292a76841086992cd7767861f6aebfedbc2d4c
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-12-01 16:52:34 -0800

    Add icon and remove anchor from Private tiles

--- frontend/app/components/DialectTile.js
+++ frontend/app/components/DialectTile.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+// Material UI
 import { withStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import LockIcon from '@material-ui/icons/Lock'
 
 import NavigationHelpers from 'common/NavigationHelpers'
-
-const { object, string, func } = PropTypes
 
 const styles = () => ({
   card: {
@@ -29,35 +30,64 @@ const styles = () => ({
     minWidth: 100,
     minHeight: 100,
   },
+  privateIcon: {
+    color: '#D8AF06',
+  },
 })
 
-function DialectTile(props) {
-  const { classes, dialectCoverImage, dialectTitle, actionIcon, dialectDescription, href, pushWindowPath } = props
+function DialectTile({
+  classes,
+  dialectCoverImage,
+  dialectTitle,
+  actionIcon,
+  dialectDescription,
+  isWorkspaces,
+  href,
+  pushWindowPath,
+}) {
+  if (href.indexOf('sections') !== -1 || isWorkspaces) {
+    return (
+      <a
+        href={NavigationHelpers.generateStaticURL(href)}
+        onClick={(e) => {
+          e.preventDefault()
+          NavigationHelpers.navigate(href, pushWindowPath, false)
+        }}
+        title={dialectTitle}
+      >
+        <Card className={`${classes.card} Dialect`}>
+          <CardMedia className={`${classes.cover} DialectCover`} image={dialectCoverImage} title={dialectTitle} />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <span className="DialectTitle fontBCSans">
+                {dialectTitle}
+                {actionIcon}
+              </span>
+              <div className="DialectDescription">{dialectDescription}</div>
+            </CardContent>
+          </div>
+        </Card>
+      </a>
+    )
+  }
   return (
-    <a
-      href={NavigationHelpers.generateStaticURL(href)}
-      onClick={(e) => {
-        e.preventDefault()
-        NavigationHelpers.navigate(href, pushWindowPath, false)
-      }}
-      title={dialectTitle}
-    >
-      <Card className={`${classes.card} Dialect`}>
-        <CardMedia className={`${classes.cover} DialectCover`} image={dialectCoverImage} title={dialectTitle} />
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <span className="DialectTitle fontBCSans">
-              {dialectTitle}
-              {actionIcon}
-            </span>
-            <div className="DialectDescription">{dialectDescription}</div>
-          </CardContent>
-        </div>
-      </Card>
-    </a>
+    <Card className={`${classes.card} Dialect`}>
+      <CardMedia className={`${classes.cover} DialectCover`} image={dialectCoverImage} title={dialectTitle} />
+
+      <div className={classes.details}>
+        <LockIcon className={classes.privateIcon} />
+        <CardContent className={classes.content}>
+          <span className="DialectTitle fontBCSans">
+            {dialectTitle}
+            {actionIcon}
+          </span>
+          <div className="DialectDescription">{dialectDescription}</div>
+        </CardContent>
+      </div>
+    </Card>
   )
 }
-
+const { bool, object, string, func } = PropTypes
 DialectTile.propTypes = {
   classes: object.isRequired,
   dialectCoverImage: string.isRequired,
@@ -65,6 +95,7 @@ DialectTile.propTypes = {
   actionIcon: object,
   dialectDescription: string,
   href: string.isRequired,
+  isWorkspaces: bool.isRequired,
   pushWindowPath: func.isRequired,
 }
 
