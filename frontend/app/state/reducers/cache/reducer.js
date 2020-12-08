923ffe1e993c41a0c423f4bfb710b2734da61ef4
Chad Oakenfold <web@oakenfold.ca>
2020-12-08 11:57:24 -0800

    Disabling local storage

--- frontend/app/state/reducers/cache/reducer.js
+++ frontend/app/state/reducers/cache/reducer.js
@@ -1,40 +1,13 @@
+// TODO: stub to temporarily disable local caching while we get a site wide invalidation system running
 import { combineReducers } from 'redux'
-
-function updateCache({ action, actionType, state }) {
-  if (action.type === actionType && action.pathOrId.toLowerCase().includes('/fv/sections')) {
-    const newState = {
-      ...state,
-      [action.pathOrId]: action,
-    }
-    return newState
-  }
-  return state
-}
-const cacheList = {
-  computeCharacters: 'FV_CHARACTERS_QUERY_SUCCESS',
-  computePortal: 'FV_PORTAL_FETCH_SUCCESS',
-}
-const cacheActions = Object.values(cacheList)
 export const cacheReducer = combineReducers({
-  // Since we aren't triggering cache specific actions we rely on updateCount to decide what to save into local cache
-  updateCount(state = 0, action) {
-    if (cacheActions.includes(action.type) && action.pathOrId.toLowerCase().includes('/fv/sections')) {
-      return state + 1
-    }
-    return state
+  updateCount() {
+    return 0
   },
-  computeCharacters(state = {}, action) {
-    return updateCache({
-      action,
-      actionType: cacheList.computeCharacters,
-      state,
-    })
+  computeCharacters() {
+    return { meta: { reducer: 'computeCharacters' } }
   },
-  computePortal(state = {}, action) {
-    return updateCache({
-      action,
-      actionType: cacheList.computePortal,
-      state,
-    })
+  computePortal() {
+    return { meta: { reducer: 'computePortal' } }
   },
 })
