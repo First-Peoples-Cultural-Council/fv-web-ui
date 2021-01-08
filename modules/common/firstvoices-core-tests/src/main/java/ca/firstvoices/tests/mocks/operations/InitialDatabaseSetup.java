b888b1f1480afe283fdddb7a1993a32a9bf14558
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-01-07 16:56:01 -0800

    Add dialect setup to InitialDatabaseSetup

--- modules/common/firstvoices-core-tests/src/main/java/ca/firstvoices/tests/mocks/operations/InitialDatabaseSetup.java
+++ modules/common/firstvoices-core-tests/src/main/java/ca/firstvoices/tests/mocks/operations/InitialDatabaseSetup.java
@@ -29,14 +29,23 @@ import static org.nuxeo.ecm.platform.usermanager.UserConfig.PASSWORD_COLUMN;
 import static org.nuxeo.ecm.platform.usermanager.UserConfig.SCHEMA_NAME;
 import static org.nuxeo.ecm.platform.usermanager.UserConfig.USERNAME_COLUMN;
 
+import ca.firstvoices.core.io.utils.StateUtils;
+import ca.firstvoices.tests.mocks.services.MockDialectService;
+import ca.firstvoices.tests.mocks.services.MockUserService;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
@@ -44,6 +53,7 @@ import org.nuxeo.ecm.core.api.security.ACE;
 import org.nuxeo.ecm.core.api.security.impl.ACLImpl;
 import org.nuxeo.ecm.core.api.security.impl.ACPImpl;
 import org.nuxeo.ecm.platform.usermanager.UserManager;
+import org.nuxeo.runtime.api.Framework;
 
 /**
  *
@@ -75,11 +85,14 @@ public class InitialDatabaseSetup {
   @Context
   protected UserManager userManager;
 
+  @Param(name = "generateData", description = "Whether or not to generate dialects and data")
+  protected Boolean generateData = false;
+
   /*
     Create the proper folder structure.
  */
   @OperationMethod
-  public void run() {
+  public void run() throws OperationException {
 
     createNewDocument("Site", "Workspace", FV_WORKSPACES);
     createNewDocument("Site", "Section", FV_SECTIONS);
@@ -164,6 +177,40 @@ public class InitialDatabaseSetup {
       userDoc.setPropertyValue(GROUPS_COLUMN, groups);
       userDoc.setProperty(SCHEMA_NAME, EMAIL_COLUMN, "@.");
       userManager.createUser(userDoc);
+    }
+
+    /*
+        Generate dialect data if requested
+     */
+    if (Boolean.TRUE.equals(generateData)) {
+      /* Create test dialects */
+      MockDialectService generateDialectService = Framework
+          .getService(MockDialectService.class);
+
+      DocumentModel privateDialect = generateDialectService
+          .generateMockDemoDialect(session, 30, "TestDialectPrivate");
+
+      DocumentModel publicDialect = generateDialectService
+          .generateMockDemoDialect(session, 30, "TestDialectPublic");
+
+      /* Generate test users for all dialects */
+      MockUserService generateDialectUsersService = Framework
+          .getService(MockUserService.class);
+
+      generateDialectUsersService
+          .generateUsersForDialects(session, userManager);
+
+      /* Publish publicDialect */
+      AutomationService automation = Framework.getService(AutomationService.class);
+      OperationContext operation = new OperationContext(session);
+      operation.setInput(publicDialect);
+      Map<String, Object> params = new HashMap<>();
+      params.put("phase", "work");
+      params.put("batchSize", 1000);
+      automation.run(operation, "Publishing.PublishDialect", params);
+
+      /* Enable privateDialect */
+      StateUtils.followTransitionIfAllowed( privateDialect, "Enable");
     }
   }
 
