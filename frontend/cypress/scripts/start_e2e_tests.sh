45aadc771cf00c9f594dec781e725265ce561b46
Daniel Yona <daniel@fpcc.ca>
2021-01-11 10:18:40 -0800

    for now, no parallel

--- frontend/cypress/scripts/start_e2e_tests.sh
+++ frontend/cypress/scripts/start_e2e_tests.sh
@@ -10,7 +10,7 @@ npm install -g wait-on
 echo "Cypress Base URL is set to {$CYPRESS_BASE_URL}"
 
 echo "*****"
-echo "Cypress CI config ($(frontend/cypress_CI.json) -> $(/e2e/cypress.json)) file content:"
+echo "Cypress CI config file content:"
 cat /e2e/cypress.json
 echo "*****"
 
@@ -19,4 +19,4 @@ wait-on "$CYPRESS_BASE_URL/nuxeo/api/v1/path/FV/sections/Data/Test/Test/TestDial
     npm ci --quiet && \
     npm run --silent cy:trashCopy && \
     npm run --silent cy:copy && \
-    cypress run --browser chrome --headless --tag "Jenkins"  --record true --parallel
+    cypress run --browser chrome --headless --tag "Jenkins" --record true
