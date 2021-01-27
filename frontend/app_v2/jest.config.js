15216d0ec7fbfac4fca5d8e2f24e759cab03b4d8
Chad Oakenfold <web@oakenfold.ca>
2021-01-27 08:48:59 -0800

    Co-locating Jest tests

--- frontend/app_v2/jest.config.js
+++ frontend/app_v2/jest.config.js
@@ -177,7 +177,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/qa/jest/**/*.test.js'],
+  testMatch: ['**/*.test.js'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
