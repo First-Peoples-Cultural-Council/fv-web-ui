0af9eb430518c860a35ffac328e330048d6ef28e
Chad Oakenfold <web@oakenfold.ca>
2021-01-26 20:26:32 -0800

    Moved React Router Provider up the tree

--- frontend/app_v2/src/bootstrap.js
+++ frontend/app_v2/src/bootstrap.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import AppFrameContainer from './components/AppFrame/AppFrameContainer'
 
-ReactDOM.render(<AppFrameContainer />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <AppFrameContainer />
+  </Router>,
+  document.getElementById('root')
+)
