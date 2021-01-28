ffaca81c6883601860a66fdf5d2ed2d8218e31b6
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-01-28 10:33:47 -0800

    Add placeholder icon as default
    - to keep styling and spacing consistent for items without icons

--- frontend/app_v2/src/common/useIcon.js
+++ frontend/app_v2/src/common/useIcon.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import AboutIcon from 'common/icons/AboutIcon'
 import ChevronDownIcon from 'common/icons/ChevronDownIcon'
+import ChevronLeftIcon from 'common/icons/ChevronLeftIcon'
 import ChevronRightIcon from 'common/icons/ChevronRightIcon'
 import CloseIcon from 'common/icons/CloseIcon'
 import DictionaryIcon from 'common/icons/DictionaryIcon'
@@ -10,7 +11,10 @@ import KidsIcon from 'common/icons/KidsIcon'
 import LearnIcon from 'common/icons/LearnIcon'
 import LoginIcon from 'common/icons/LoginIcon'
 import Logo from 'common/icons/Logo'
+import PlaceHolderIcon from 'common/icons/PlaceHolderIcon'
 import ResourcesIcon from 'common/icons/ResourcesIcon'
+
+// a helper function that given a string name returns an icon, if no string is supplied it will return a blank placeholder icon
 
 function useIcon(iconName, iconStyling) {
   const styling = iconStyling ? iconStyling : 'fill-current h-12 w-8'
@@ -21,6 +25,8 @@ function useIcon(iconName, iconStyling) {
       return <DictionaryIcon styling={styling} />
     case 'ChevronDown':
       return <ChevronDownIcon styling={styling} />
+    case 'ChevronLeft':
+      return <ChevronLeftIcon styling={styling} />
     case 'ChevronRight':
       return <ChevronRightIcon styling={styling} />
     case 'Close':
@@ -38,7 +44,7 @@ function useIcon(iconName, iconStyling) {
     case 'Resources':
       return <ResourcesIcon styling={styling} />
     default:
-      return null
+      return <PlaceHolderIcon styling={styling} />
   }
 }
 export default useIcon
