90b3a386145a7e31fdff3c0512b9c680335b95ff
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-02-01 15:16:20 -0800

    Handle no audio src

--- frontend/app_v2/src/components/WidgetWotd/WidgetWotdPresentation.js
+++ frontend/app_v2/src/components/WidgetWotd/WidgetWotdPresentation.js
@@ -8,6 +8,8 @@ import Share from 'components/Share'
  * @component
  *
  * @param {object} props
+ * @param {object} entry - the document to be featured: should include title, url, definition, and optional audio src.
+ * @param {function} onAudioClick - audio icon click handler
  *
  * @returns {node} jsx markup
  */
@@ -18,9 +20,11 @@ function WidgetWotdPresentation({ entry, onAudioClick }) {
         <h2 className="mb-12 text-4xl text-fv-blue font-bold uppercase sm:text-5xl">Word of the Day</h2>
         <p className="mt-2 max-w-2xl inline-flex items-center text-4xl font-bold text-black md:mx-auto sm:text-5xl">
           <a href={entry.url}>{entry.title}</a>
-          <button onClick={() => onAudioClick()} className="ml-4 inline">
-            {useIcon('Audio', 'h-9 w-9 sm:w-12 sm:h-12')}
-          </button>
+          {entry.audioSrc ? (
+            <button onClick={() => onAudioClick()} className="ml-4 inline">
+              {useIcon('Audio', 'h-9 w-9 sm:w-12 sm:h-12')}
+            </button>
+          ) : null}
         </p>
         <p className="mt-4 max-w-2xl text-2xl text-gray-500 md:mx-auto sm:text-3xl">{entry.definition}</p>
         <h3 className="mt-8 max-w-2xl text-lg text-fv-red md:mx-auto sm:text-xl">Share on:</h3>
