e6f3735a86cc68fbdbe950575a103b961bb92636
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-02-01 11:33:04 -0800

    CSS adjustments

--- frontend/app_v2/src/components/WidgetWotd/WidgetWotdData.js
+++ frontend/app_v2/src/components/WidgetWotd/WidgetWotdData.js
@@ -11,10 +11,12 @@ import PropTypes from 'prop-types'
  */
 function WidgetWotdData({ children }) {
   const entry = {
-    title: "adelht̲s̲'ine",
+    title: "ali'wadzāgwis",
     definition:
       'the people sitting at a potlatch, that is, the members of the invited clans as opposed to the host clan.',
     audioFile: '',
+    url:
+      'https://www.firstvoices.com/explore/FV/sections/Data/Athabascan/Yekooche/Yekooche/learn/words/167adc92-020c-4fa8-94a3-3cdf18ac05ae',
   }
   return children({
     entry,
--- frontend/app_v2/src/components/WidgetWotd/WidgetWotdPresentation.js
+++ frontend/app_v2/src/components/WidgetWotd/WidgetWotdPresentation.js
@@ -13,21 +13,16 @@ import Share from 'components/Share'
  */
 function WidgetWotdPresentation({ entry }) {
   return (
-    <section className="py-12 bg-white">
+    <section className="mx-6 py-12 bg-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <h2 className="m-12 text-4xl text-fv-blue font-bold tracking-wide uppercase sm:text-5xl">Word of the Day</h2>
-        <p className="mt-2 text-4xl leading-8 font-bold tracking-tight text-black sm:text-5xl">
+        <h2 className="mb-12 text-4xl text-fv-blue font-bold uppercase sm:text-5xl">Word of the Day</h2>
+        <p className="mt-2 max-w-2xl text-4xl font-bold text-black md:mx-auto sm:text-5xl">
           {entry.title}
-          {useIcon('Resources', 'ml-4 h-10 w-10 inline')}
+          {useIcon('Audio', 'ml-4 inline h-9 w-9 sm:w-12 sm:h-12')}
         </p>
         <p className="mt-4 max-w-2xl text-2xl text-gray-500 md:mx-auto sm:text-3xl">{entry.definition}</p>
-        <h3 className="mt-4 max-w-2xl text-2xl text-fv-red md:mx-auto sm:text-3xl">Share on:</h3>
-        <Share.Container
-          url={
-            'https://www.firstvoices.com/explore/FV/sections/Data/Athabascan/Yekooche/Yekooche/learn/words/167adc92-020c-4fa8-94a3-3cdf18ac05ae'
-          }
-          title={'Tester'}
-        />
+        <h3 className="mt-8 max-w-2xl text-lg text-fv-red md:mx-auto sm:text-xl">Share on:</h3>
+        <Share.Container url={entry.url} title={entry.title} />
       </div>
     </section>
   )
