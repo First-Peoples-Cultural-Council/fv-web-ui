cda4f5eb9eb1e6b4af70a9f3c6bac899f5723191
Chad Oakenfold <web@oakenfold.ca>
2021-02-02 16:21:06 -0800

    Adds icons

--- frontend/app_v2/src/common/useIcon.js
+++ frontend/app_v2/src/common/useIcon.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import AboutIcon from 'common/icons/AboutIcon'
+import Book from 'common/icons/Book'
+import ChatBubble from 'common/icons/ChatBubble'
 import ChevronDownIcon from 'common/icons/ChevronDownIcon'
 import ChevronLeftIcon from 'common/icons/ChevronLeftIcon'
 import ChevronRightIcon from 'common/icons/ChevronRightIcon'
@@ -9,42 +11,45 @@ import DictionaryIcon from 'common/icons/DictionaryIcon'
 import HamburgerMenuIcon from 'common/icons/HamburgerMenuIcon'
 import KidsIcon from 'common/icons/KidsIcon'
 import LearnIcon from 'common/icons/LearnIcon'
+import Lessons from 'common/icons/Lessons'
 import LoginIcon from 'common/icons/LoginIcon'
 import Logo from 'common/icons/Logo'
+import MusicNote from 'common/icons/MusicNote'
 import PlaceHolderIcon from 'common/icons/PlaceHolderIcon'
+import Quote from 'common/icons/Quote'
 import ResourcesIcon from 'common/icons/ResourcesIcon'
+import PlayCircle from 'common/icons/PlayCircle'
+import PauseCircle from 'common/icons/PauseCircle'
+import TimesCircle from 'common/icons/TimesCircle'
 
 // a helper function that given a string name returns an icon, if no string is supplied it will return a blank placeholder icon
 
 function useIcon(iconName, iconStyling) {
   const styling = iconStyling ? iconStyling : 'fill-current h-12 w-8'
-  switch (iconName) {
-    case 'About':
-      return <AboutIcon styling={styling} />
-    case 'Dictionary':
-      return <DictionaryIcon styling={styling} />
-    case 'ChevronDown':
-      return <ChevronDownIcon styling={styling} />
-    case 'ChevronLeft':
-      return <ChevronLeftIcon styling={styling} />
-    case 'ChevronRight':
-      return <ChevronRightIcon styling={styling} />
-    case 'Close':
-      return <CloseIcon styling={styling} />
-    case 'HamburgerMenu':
-      return <HamburgerMenuIcon styling={styling} />
-    case 'Kids':
-      return <KidsIcon styling={styling} />
-    case 'Learn':
-      return <LearnIcon styling={styling} />
-    case 'Login':
-      return <LoginIcon styling={styling} />
-    case 'Logo':
-      return <Logo styling={styling} />
-    case 'Resources':
-      return <ResourcesIcon styling={styling} />
-    default:
-      return <PlaceHolderIcon styling={styling} />
+  const icons = {
+    About: AboutIcon,
+    Book,
+    ChatBubble,
+    ChevronDown: ChevronDownIcon,
+    ChevronLeft: ChevronLeftIcon,
+    ChevronRight: ChevronRightIcon,
+    Close: CloseIcon,
+    Dictionary: DictionaryIcon,
+    HamburgerMenu: HamburgerMenuIcon,
+    Kids: KidsIcon,
+    Learn: LearnIcon,
+    Lessons,
+    Login: LoginIcon,
+    Logo,
+    MusicNote,
+    Quote,
+    Resources: ResourcesIcon,
+    PlayCircle,
+    PauseCircle,
+    TimesCircle,
   }
+  const iconFile = icons[iconName]
+  const Icon = iconFile ? iconFile : PlaceHolderIcon
+  return <Icon styling={styling} />
 }
 export default useIcon
