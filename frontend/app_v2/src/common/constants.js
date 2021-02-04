deb18f8a1784f5b40527f0c5d8433a17bbbeb4de
Chad Oakenfold <web@oakenfold.ca>
2021-02-04 09:18:01 -0800

    Adds constants

--- frontend/app_v2/src/common/constants.js
+++ frontend/app_v2/src/common/constants.js
@@ -1,7 +1,12 @@
+export const PAGE_NAVIGATION = 'PAGE_NAVIGATION'
+
 export const AUDIO_ERRORED = 'AUDIO_ERRORED'
 export const AUDIO_UNLOADED = 'AUDIO_UNLOADED'
 export const AUDIO_LOADING = 'AUDIO_LOADING'
 export const AUDIO_LOADED = 'AUDIO_LOADED'
 export const AUDIO_STOPPED = 'AUDIO_STOPPED'
 export const AUDIO_PLAYING = 'AUDIO_PLAYING'
-export const PAGE_NAVIGATION = 'PAGE_NAVIGATION'
+
+export const HEROWIDGET = 'HeroWidget'
+export const SCHEDULEWIDGET = 'ScheduleWidget'
+export const LISTWIDGET = 'ListWidget'
