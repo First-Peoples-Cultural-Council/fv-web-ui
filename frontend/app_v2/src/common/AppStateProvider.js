1ea0d2d0c097b9021af7adb91469ac6618fa1a14
Chad Oakenfold <web@oakenfold.ca>
2021-02-08 10:59:34 -0800

    Adds reducer to AppStateContext.Provider

--- frontend/app_v2/src/common/AppStateProvider.js
+++ frontend/app_v2/src/common/AppStateProvider.js
@@ -1,12 +1,47 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useReducer } from 'react'
 
 import AppStateContext from 'common/AppStateContext'
 import AudioMachineData from 'components/AudioMachine/AudioMachineData'
 
+function reducer(state, action) {
+  if (action.type === 'api.getSections') {
+    state.api.getSections = action.payload
+  }
+  return state
+
+  // switch (action.type) {
+  //   case 'api.getSections': {
+  //     state.api.getSections = action.payload
+  //     return state
+  //   }
+  //   default:
+  //     return state
+  // }
+}
 function AppStateProvider({ children }) {
   const { machine, send } = AudioMachineData()
-  return <AppStateContext.Provider value={{ audio: { machine, send } }}>{children}</AppStateContext.Provider>
+  const [state, dispatch] = useReducer(reducer, {
+    api: {
+      getSections: {},
+    },
+  })
+  return (
+    <AppStateContext.Provider
+      value={{
+        reducer: {
+          state,
+          dispatch,
+        },
+        audio: {
+          machine,
+          send,
+        },
+      }}
+    >
+      {children}
+    </AppStateContext.Provider>
+  )
 }
 // PROPTYPES
 const { node } = PropTypes
