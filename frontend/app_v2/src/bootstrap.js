86dc1e23d6c0d4663c5a4e41cfcb2c6a162f4d7c
Chad Oakenfold <web@oakenfold.ca>
2021-02-10 12:12:04 -0800

    Bootstrap drops unneeded Switch router component

--- frontend/app_v2/src/bootstrap.js
+++ frontend/app_v2/src/bootstrap.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import AppV1Provider from 'app_v1/FVProvider'
 import 'tailwindcss/tailwind.css'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -13,13 +13,11 @@ ReactDOM.render(
   <AppV1Provider>
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Switch>
-          <Route path="/:language">
-            <AppStateProvider>
-              <AppFrameContainer />
-            </AppStateProvider>
-          </Route>
-        </Switch>
+        <Route path="/:language">
+          <AppStateProvider>
+            <AppFrameContainer />
+          </AppStateProvider>
+        </Route>
       </Router>
     </QueryClientProvider>
   </AppV1Provider>,
