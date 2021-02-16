ac5cadbf2c519b2c669cd228f7801e2ed9e86008
Chad Oakenfold <web@oakenfold.ca>
2021-02-16 15:47:53 -0800

    CircleImage prop doc & isRequired

--- frontend/app_v2/src/components/CircleImage/CircleImagePresentation.js
+++ frontend/app_v2/src/components/CircleImage/CircleImagePresentation.js
@@ -18,13 +18,16 @@ function CircleImagePresentation({ src, alt, classNameWidth, classNameHeight }) 
 // PROPTYPES
 const { string } = PropTypes
 CircleImagePresentation.propTypes = {
-  src: string,
-  alt: string,
+  /** Url to image */
+  src: string.isRequired,
+  /** Alt text for image */
+  alt: string.isRequired,
+  /** Change the width using Tailwind CSS class, eg: `classNameWidth="w-56 lg:w-72"` */
   classNameWidth: string,
+  /** Change the height using Tailwind CSS class, eg: `classNameHeight="h-56 lg:h-72"` */
   classNameHeight: string,
 }
 CircleImagePresentation.defaultProps = {
-  alt: '',
   classNameWidth: 'w-24',
   classNameHeight: 'h-24',
 }
---
+++ frontend/app_v2/src/components/CircleImage/CircleImagePresentation.md
@@ -0,0 +1,8 @@
+```jsx padded
+<CircleImagePresentation
+  src="https://dev.firstvoices.com/nuxeo/nxfile/default/51ac02d0-27c3-44c6-8674-b13699369cd5/picture:views/2/content/Medium_TestPhoto.jpg"
+  classNameHeight="h-56 lg:h-72"
+  classNameWidth="w-56 lg:w-72"
+  alt="TestPhoto"
+/>
+```
