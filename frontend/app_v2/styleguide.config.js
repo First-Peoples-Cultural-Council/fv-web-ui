f2a1c75e3736d4cb7f5816549ef2ddb21a500462
Chad Oakenfold <web@oakenfold.ca>
2021-02-16 15:44:20 -0800

    Adds Tailwinds to Styleguidist

--- frontend/app_v2/styleguide.config.js
+++ frontend/app_v2/styleguide.config.js
@@ -1,5 +1,6 @@
 const alias = require('./webpack/webpack.alias')
 module.exports = {
+  require: ['dist/tailwind.css'],
   components: 'src/components/!(_TEMPLATE|AppFrame)*/!(index)+(Presentation)*.js',
   webpackConfig: require('./webpack/webpack.styleguidist.js'),
   moduleAliases: alias,
