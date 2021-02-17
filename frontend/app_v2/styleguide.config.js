3d1592e8661bda1527d065d3a99825d6c3a46b7c
Chad Oakenfold <web@oakenfold.ca>
2021-02-17 12:37:08 -0800

    Adds copyfiles dep, builds assets for styleguide

--- frontend/app_v2/styleguide.config.js
+++ frontend/app_v2/styleguide.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const alias = require('./webpack/webpack.alias')
 
 module.exports = {
-  require: ['dist/tailwind.css'],
+  require: ['dist/styleguide/tailwind.css'],
   components: 'src/components/!(_TEMPLATE|AppFrame)*/!(index)+(Presentation)*.js',
   webpackConfig: require('./webpack/webpack.styleguidist.js'),
   moduleAliases: alias,
