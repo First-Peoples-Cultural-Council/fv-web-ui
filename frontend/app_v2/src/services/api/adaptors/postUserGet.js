8d57465443956740e73791f858ec1037dba158f1
Chad Oakenfold <web@oakenfold.ca>
2021-02-23 15:46:54 -0800

    Adaptors: new & externalized existing

---
+++ frontend/app_v2/src/services/api/adaptors/postUserGet.js
@@ -0,0 +1,19 @@
+const postUserGet = (response) => {
+  const { properties } = response
+  const userPreferences = [
+    { type: 'display', value: 'dark' },
+    { type: 'workspaceToggle', value: true },
+  ]
+  const workspacePreferences = userPreferences.find(({ type }) => type === 'workspaceToggle')
+  const isWorkspaceOn = workspacePreferences ? workspacePreferences.value : false
+  return {
+    userPreferences,
+    // userPreferences: properties?.['user:preferences'], // TODO: ENABLE WHEN ENDPOINT UPDATED
+    firstName: properties?.['user:firstName'],
+    lastName: properties?.['user:lastName'],
+    groups: properties?.['user:groups'],
+    userName: properties?.['user:username'],
+    isWorkspaceOn,
+  }
+}
+export default postUserGet
