0a9ce3216bd718d329900646ccafc5db073318d5
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-03-05 15:45:21 -0800

    Remove comments

--- frontend/app_v2/src/components/MenuMachine/MenuMachineData.js
+++ frontend/app_v2/src/components/MenuMachine/MenuMachineData.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { useMachine } from '@xstate/react'
 import { useLocation } from 'react-router-dom'
-// import { AUDIO_ERRORED, AUDIO_LOADED, AUDIO_STOPPED, PAGE_NAVIGATION } from 'common/constants'
 import menuMachine from 'components/MenuMachine/menuMachine'
 /**
  * @summary MenuMachineData
@@ -13,7 +12,6 @@ import menuMachine from 'components/MenuMachine/menuMachine'
  */
 function MenuMachineData() {
   const [machine, send] = useMachine(menuMachine)
-  const { openMenu } = machine.context
   const location = useLocation()
   useEffect(() => {
     send('CLOSE')
