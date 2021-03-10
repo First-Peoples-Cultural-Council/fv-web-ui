cf05f222d7635f0ecd8cab91ab8e82b809c04d16
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-03-10 11:35:46 -0800

    Search box styling

--- frontend/app_v2/src/components/Home/HomePresentation.js
+++ frontend/app_v2/src/components/Home/HomePresentation.js
@@ -76,9 +76,15 @@ function HomePresentation({ isLoading, error, data, language }) {
                 foreground={<h1 className="font-medium text-2xl">{language.title}</h1>}
                 foregroundIcon={foregroundIcon}
                 search={
-                  <div className="bg-white flex px-8 py-4 rounded-25 w-3/4 text-fv-charcoal-light">
-                    <button type="button">{useIcon('Search', 'fill-current h-8 w-8 ')}</button>
-                    <input className="ml-6 w-full text-2xl" type="text" placeholder={`Search ${language.title}`} />
+                  <div className="bg-white flex rounded-2xl w-3/5 text-fv-charcoal-light p-1 divide-x-2 divide-gray-300">
+                    <input
+                      className="w-full foucus text-2xl px-4 py-2 "
+                      type="text"
+                      placeholder={`Search ${language.title}`}
+                    />
+                    <button type="button " className="p-2">
+                      {useIcon('Search', 'fill-current h-8 w-8 ')}
+                    </button>
                   </div>
                 }
                 variant={variant}
