620477eb06b3ad8067a63f6d7e26322bd9858060
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-04-06 16:36:01 -0700

    Add Home and Support links

--- frontend/app_v2/src/components/ErrorHandler/ErrorHandlerPresentation.js
+++ frontend/app_v2/src/components/ErrorHandler/ErrorHandlerPresentation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 // FPCC
 import useIcon from 'common/useIcon'
@@ -18,13 +18,13 @@ function ErrorHandlerPresentation({ status, heading, content }) {
     <div className="min-w-screen min-h-screen bg-gradient-to-r from-fv-turquoise to-fv-blue-light flex items-center p-5 lg:p-20 overflow-hidden relative">
       <div className="w-full flex-1 min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative items-center text-center">
         {useIcon('Logo', 'h-24 mb-10 mx-auto')}
-        <div className="mb-10 md:mb-20 text-gray-600 font-light">
+        <div className="mb-10 md:mb-16 text-gray-600 font-light">
           <h1 className="font-black uppercase text-3xl lg:text-5xl text-fv-turquoise mb-10">
             {status} {heading}
           </h1>
           {content}
         </div>
-        <div className="mb-20 md:mb-0">
+        <div className="mb-4 md:mb-8">
           <button
             className="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-fv-turquoise hover:text-fv-turquoise-dark"
             onClick={() => history.goBack()}
@@ -33,6 +33,18 @@ function ErrorHandlerPresentation({ status, heading, content }) {
             Go Back
           </button>
         </div>
+        <Link
+          className="bg-gradient-to-r from-fv-orange to-fv-red hover:from-fv-red hover:to-fv-red text-white font-semibold px-6 py-3 rounded-md mr-6"
+          to="/"
+        >
+          Home
+        </Link>
+        <a
+          className="bg-gradient-to-r from-fv-blue-light to-fv-turquoise hover:from-fv-blue-light hover:to-fv-blue-light text-white font-semibold px-6 py-3 rounded-md"
+          href="https://support.firstvoices.com/servicedesk/customer/portal/1"
+        >
+          Support
+        </a>
       </div>
     </div>
   )
