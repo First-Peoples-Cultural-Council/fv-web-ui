a0fbcc927a695a96c3deb3f4ece26fda2daae458
Daniel Yona <daniel@fpcc.ca>
2021-04-09 13:37:50 -0700

    Remove additional legacy tasks code

--- frontend/app/state/reducers/tasks/reducer.js
+++ frontend/app/state/reducers/tasks/reducer.js
@@ -32,8 +32,6 @@ const initialState = {
 
 const computeUserTasksOperation = computeOperation('user_tasks')
 const computeTasksOperation = computeOperation('tasks')
-const computeUserTasksApproveOperation = computeOperation('user_tasks_approve')
-const computeUserTasksRejectOperation = computeOperation('user_tasks_reject')
 const computeCountTotalTasksFactory = computeOperation('count_total_tasks')
 const computeUserRegistrationTasksFactory = computeOperation('user_registration')
 const computeUserRegistrationApproveOperation = computeOperation('user_registration_approve')
@@ -197,8 +195,6 @@ export const tasksReducer = combineReducers({
   },
   computeTasks: computeTasksOperation.computeTasks,
   computeUserTasks: computeUserTasksOperation.computeUserTasks,
-  computeUserTasksApprove: computeUserTasksApproveOperation.computeUserTasksApprove,
-  computeUserTasksReject: computeUserTasksRejectOperation.computeUserTasksReject,
   computeCountTotalTasks: computeCountTotalTasksFactory.computeCountTotalTasks,
   computeUserRegistrationTasks: computeUserRegistrationTasksFactory.computeUserRegistration,
   computeUserRegistrationApprove: computeUserRegistrationApproveOperation.computeUserRegistrationApprove,
