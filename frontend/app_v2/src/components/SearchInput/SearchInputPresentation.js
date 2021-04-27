6104393936f1c5739e846a790377dddc62fd87b3
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-04-27 14:50:44 -0700

    Change SearchInput div to form
    - fix positioning of select menu

--- frontend/app_v2/src/components/SearchInput/SearchInputPresentation.js
+++ frontend/app_v2/src/components/SearchInput/SearchInputPresentation.js
@@ -49,39 +49,34 @@ function SearchInputPresentation({
   return (
     <>
       <div className="flex rounded-md">
-        <div className="relative flex items-stretch flex-grow focus-within:z-10">
+        <form onSubmit={handleSearchSubmit} className="flex items-stretch flex-grow">
           <input
             data-testid="SearchInput"
             className="block w-full focus text-2xl text-fv-charcoal-light rounded-none rounded-l-md pl-4"
             type="text"
             placeholder={`Search ${siteTitle}`}
             onChange={handleTextFieldChange}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleSearchSubmit(e)
-              }
-            }}
             value={searchValue}
           />
-        </div>
-        <div className="-ml-px relative inline-flex items-center px-2 py-1.5 border-l-2 text-fv-charcoal-light border-gray-300 text-sm font-medium rounded-r-md bg-gray-50 hover:bg-gray-100">
+        </form>
+        <div className="relative inline-flex items-center px-2 py-1.5 text-fv-charcoal-light border-l-2 border-gray-300 text-sm font-medium rounded-r-md bg-gray-50 hover:bg-gray-100">
           <label htmlFor="search-options" className="sr-only">
             Search options
           </label>
           <button
             type="button"
             onClick={onSearchOptionsClick}
-            className="relative rounded-md pl-3 pr-16 py-2 mr-1 border-fv-blue text-left cursor-default sm:text-sm"
+            className="w-24 relative rounded-md pl-3 pr-16 py-2 mr-1 text-left cursor-default sm:text-sm"
             aria-haspopup="listbox"
           >
             {currentOption.label}
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              {useIcon('UnfoldMore', 'h-5 w-5')}
+              {useIcon('ChevronDown', 'h-5 w-5')}
             </span>
           </button>
           {isMenuOpen ? (
             <ul
-              className="transition-opacity ease-in duration-100 absolute mt-1.5 py-1 w-full bg-white shadow-lg max-h-60 rounded-md text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+              className="absolute top-14 left-0 z-10 py-1 transition-opacity ease-in duration-100 w-full bg-white shadow-lg max-h-60 rounded-md text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
               tabIndex={-1}
               role="listbox"
               ref={menuRef}
