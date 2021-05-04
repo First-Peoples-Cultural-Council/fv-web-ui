c4b9df3fd669d3a283cd1d0832904da7e9f81c95
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-05-03 17:06:05 -0700

    Update Phrase data layer

--- frontend/app_v2/src/components/Phrase/PhraseData.js
+++ frontend/app_v2/src/components/Phrase/PhraseData.js
@@ -1,5 +1,12 @@
+import { useEffect } from 'react'
+import { useQuery } from 'react-query'
+import { useHistory, useParams } from 'react-router-dom'
+
 import useGetSite from 'common/useGetSite'
-import { useParams } from 'react-router-dom'
+import documentApi from 'services/api/document'
+import { triggerError } from 'common/navigationHelpers'
+import { localDateMDYT } from 'common/stringHelpers'
+
 /**
  * @summary PhraseData
  * @component
@@ -8,12 +15,57 @@ import { useParams } from 'react-router-dom'
  *
  */
 function PhraseData() {
-  const site = useGetSite()
   const { phraseId } = useParams()
+  const { path } = useGetSite()
+  const history = useHistory()
+  const siteShortUrl = path ? path.split('/').pop() : ''
+
+  // Data fetch
+  const response = useQuery(
+    ['phrase', phraseId],
+    () => documentApi.get({ id: phraseId, contextParameters: 'phrase' }),
+    {
+      // The query will not execute until the phraseId has been provided
+      enabled: !!phraseId,
+    }
+  )
+  const { data, error, isError, isLoading } = response
+
+  const properties = data?.properties ? data.properties : {}
+  const phraseContextParams = data?.contextParameters?.phrase ? data.contextParameters.phrase : {}
+
+  const entry = {
+    title: properties['dc:title'] || '',
+    docType: data?.type || '',
+    translations: properties['fv:definitions'] || [],
+    acknowledgement: properties['fv-phrase:acknowledgement'] || '',
+    culturalNotes: properties['fv:cultural_note'] || [],
+    generalNote: properties['fv:general_note'] || [],
+    pronunciation: properties['fv-phrase:pronunciation'] || '',
+    reference: properties['fv:reference'] || '',
+    created: localDateMDYT(properties['dc:created']) || '',
+    modified: localDateMDYT(properties['dc:modified']) || '',
+    version: properties['uid:major_version'] || '',
+    categories: phraseContextParams?.phrase_books || [],
+    audio: phraseContextParams?.related_audio || [],
+    relatedPhrases: phraseContextParams?.related_phrases || [],
+    relatedAssets: phraseContextParams?.related_assets || [],
+    pictures: phraseContextParams?.related_pictures || [],
+    videos: phraseContextParams?.related_videos || [],
+    sources: phraseContextParams?.sources || [],
+  }
+
+  useEffect(() => {
+    if (isError) triggerError(error, history)
+  }, [isError])
 
   return {
-    hasSiteData: site?.title !== undefined,
     phraseId,
+    isLoading,
+    entry: data?.title ? entry : {},
+    actions: ['copy', 'share'],
+    moreActions: ['copy'],
+    siteShortUrl,
   }
 }
 
