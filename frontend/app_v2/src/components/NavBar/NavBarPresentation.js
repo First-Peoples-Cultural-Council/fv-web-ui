d844e2ad55787b47bb4c51667faf82c79d129895
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-05-06 09:17:58 -0700

    Centre menus when logged in

--- frontend/app_v2/src/components/NavBar/NavBarPresentation.js
+++ frontend/app_v2/src/components/NavBar/NavBarPresentation.js
@@ -59,7 +59,7 @@ function NavBarPresentation({
   return (
     <header id="NavBar" className={`relative bg-fv-charcoal ${className}`} onKeyUp={onKeyPress}>
       <nav className="max-w-screen-2xl mx-auto px-2 lg:px-6 xl:px-16">
-        <div className="flex justify-between items-center py-1  md:space-x-10">
+        <div className="flex justify-between items-center py-1  lg:space-x-10">
           <div className="flex items-center">
             <Link className="text-white flex items-center" to={'/'}>
               <span className="sr-only">FirstVoices Logo</span>
@@ -78,36 +78,34 @@ function NavBarPresentation({
           </div>
 
           <div className="hidden lg:flex xl:space-x-6 ">{menus}</div>
-
-          {currentUser?.username === 'Guest' || currentUser?.username === undefined ? (
-            <div className="hidden lg:flex items-center">
-              <a
-                href="/nuxeo/logout?requestedUrl=login.jsp"
-                className="whitespace-nowrap text-lg font-medium text-white hover:text-gray-100"
+          <div className="inline-flex">
+            {currentUser?.username === 'Guest' || currentUser?.username === undefined ? (
+              <div className="hidden lg:flex items-center">
+                <a
+                  href="/nuxeo/logout?requestedUrl=login.jsp"
+                  className="whitespace-nowrap text-lg font-medium text-white hover:text-gray-100"
+                >
+                  Sign in
+                </a>
+                <a
+                  href="/register?requestedUrl=/register"
+                  className="hidden ml-4 whitespace-nowrap xl:inline-flex items-center justify-center py-2 px-4 border border-transparent rounded-3xl  shadow-sm text-base font-medium text-white bg-fv-orange hover:bg-fv-orange-dark"
+                >
+                  Register
+                </a>
+              </div>
+            ) : (
+              <div
+                onClick={(event) => {
+                  onClickOutside(event, userMenuId)
+                }}
+                onMouseDown={(event) => {
+                  if (openMenu === userMenuId) {
+                    event.stopPropagation()
+                  }
+                }}
               >
-                Sign in
-              </a>
-              <a
-                href="/register?requestedUrl=/register"
-                className="hidden ml-4 whitespace-nowrap xl:inline-flex items-center justify-center py-2 px-4 border border-transparent rounded-3xl  shadow-sm text-base font-medium text-white bg-fv-orange hover:bg-fv-orange-dark"
-              >
-                Register
-              </a>
-            </div>
-          ) : (
-            <div
-              className="relative ml-8 flex flex-1 lg:w-0"
-              onClick={(event) => {
-                onClickOutside(event, userMenuId)
-              }}
-              onMouseDown={(event) => {
-                if (openMenu === userMenuId) {
-                  event.stopPropagation()
-                }
-              }}
-            >
-              {/* User Avatar */}
-              <div className="ml-4 flex items-center md:ml-6">
+                {/* User Avatar */}
                 <button
                   className="max-w-xs p-3 bg-fv-orange hover:bg-fv-orange-dark text-white text-xl rounded-full h-12 w-12 flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-fv-turquoise"
                   id="user-menu"
@@ -124,43 +122,43 @@ function NavBarPresentation({
                   <span className="sr-only">Open user menu</span>
                   {currentUser.userInitials}
                 </button>
-              </div>
-              {/* User Menu dropdown */}
-              {openMenu === userMenuId ? (
-                <div className="absolute mt-8 w-72 py-8 transform lg:-translate-x-0" role="menu">
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <ul className="text-base text-fv-charcoal font-medium grid bg-white gap-12 p-8 md:gap-8">
-                      <li className="-m-3 py-1  hover:bg-gray-100" role="menuitem">
-                        <Link to="/dashboard">Dashboard</Link>
-                      </li>
-                      <li className="whitespace-nowrap -m-3 py-1 hover:bg-gray-100">
-                        <label htmlFor="toggle" className="inline-block">
-                          Workspace Mode
-                        </label>
-                        <FVToggle
-                          toggled={workspaceToggleValue}
-                          toggleCallback={onWorkspaceModeClick}
-                          styling={'ml-6 inline-block align-middle'}
-                        />
-                      </li>
-                      <li className="-m-3 py-1 hover:bg-gray-100" role="menuitem">
-                        <a href="/nuxeo/logout">Sign out</a>
-                      </li>
-                    </ul>
+                {/* User Menu dropdown */}
+                {openMenu === userMenuId ? (
+                  <div className="absolute top-6 right-0 w-72 py-8 transform lg:-translate-x-0" role="menu">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                      <ul className="text-base text-fv-charcoal font-medium grid bg-white gap-12 p-8 md:gap-8">
+                        <li className="-m-3 py-1  hover:bg-gray-100" role="menuitem">
+                          <Link to="/dashboard">Dashboard</Link>
+                        </li>
+                        <li className="whitespace-nowrap -m-3 py-1 hover:bg-gray-100">
+                          <label htmlFor="toggle" className="inline-block">
+                            Workspace Mode
+                          </label>
+                          <FVToggle
+                            toggled={workspaceToggleValue}
+                            toggleCallback={onWorkspaceModeClick}
+                            styling={'ml-6 inline-block align-middle'}
+                          />
+                        </li>
+                        <li className="-m-3 py-1 hover:bg-gray-100" role="menuitem">
+                          <a href="/nuxeo/logout">Sign out</a>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                </div>
-              ) : null}
+                ) : null}
+              </div>
+            )}
+            <div className="items-center lg:hidden">
+              <button
+                type="button"
+                onClick={() => openCloseMobileNavbar()}
+                className="bg-fv-charcoal rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-100 focus:outline-none"
+              >
+                <span className="sr-only">{mobileNavbarOpen ? 'Close menu' : 'Open menu'}</span>
+                {mobileNavbarOpen ? useIcon('Close', 'h-6 w-6') : useIcon('HamburgerMenu', 'h-6 w-6')}
+              </button>
             </div>
-          )}
-          <div className="-mr-2 -my-2 lg:hidden">
-            <button
-              type="button"
-              onClick={() => openCloseMobileNavbar()}
-              className="bg-fv-charcoal rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-100 focus:outline-none"
-            >
-              <span className="sr-only">{mobileNavbarOpen ? 'Close menu' : 'Open menu'}</span>
-              {mobileNavbarOpen ? useIcon('Close', 'h-6 w-6') : useIcon('HamburgerMenu', 'h-6 w-6')}
-            </button>
           </div>
         </div>
       </nav>
