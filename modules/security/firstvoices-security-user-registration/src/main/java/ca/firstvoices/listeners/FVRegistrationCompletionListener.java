23b91295739d2f7873e7f23b7b6a238eec768b28
Daniel Yona <daniel@fpcc.ca>
2021-06-08 12:55:44 -0700

    Cleanup code

--- modules/security/firstvoices-security-user-registration/src/main/java/ca/firstvoices/listeners/FVRegistrationCompletionListener.java
+++ modules/security/firstvoices-security-user-registration/src/main/java/ca/firstvoices/listeners/FVRegistrationCompletionListener.java
@@ -20,15 +20,9 @@
 
 package ca.firstvoices.listeners;
 
-import static ca.firstvoices.utils.FVRegistrationConstants.GROUP_NAME_ARG;
 import static ca.firstvoices.utils.FVRegistrationConstants.INVITATION_VALIDATED;
-import static ca.firstvoices.utils.FVRegistrationConstants.LADMIN_APPROVED_GROUP_CHANGE;
 import static ca.firstvoices.utils.FVRegistrationConstants.MEMBERS;
-import static ca.firstvoices.utils.FVRegistrationConstants.SYSTEM_APPROVED_GROUP_CHANGE;
-import static ca.firstvoices.utils.FVRegistrationConstants.USER_NAME_ARG;
 
-import ca.firstvoices.services.FVMoveUserToDialectServiceImpl;
-import ca.firstvoices.utils.FVRegistrationUtilities;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
@@ -46,14 +40,14 @@ import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.runtime.api.Framework;
 
 /**
- *
+ * This listener will handles `invitationValidated` events, which are sent when a user
+ * creates a password and their registration request is converted to a user account.
+ * `invitationValidated` is sent from nuxeo
  */
 public class FVRegistrationCompletionListener implements EventListener {
 
   private static final Log log =
       LogFactory.getLog(ca.firstvoices.listeners.FVRegistrationCompletionListener.class);
-
-  // accepts documentRemoved && registrationValidated
 
   @Override
   public void handleEvent(Event event) {
@@ -65,64 +59,12 @@ public class FVRegistrationCompletionListener implements EventListener {
 
     DocumentEventContext docCtx = (DocumentEventContext) ctx;
 
-    FVRegistrationUtilities regUtil = new FVRegistrationUtilities();
-    FVMoveUserToDialectServiceImpl util = new FVMoveUserToDialectServiceImpl();
-    Object[] args;
-    DocumentModel dialect;
+    if (INVITATION_VALIDATED.equals(event.getName())) {
+      DocumentModel registrationDoc = docCtx.getSourceDocument();
 
-    switch (event.getName()) {
-      // TODO: this event is not triggered yet
-      // TODO: should be triggered when administrator approves a member to join Private
-      //  (Enabled) dialect.
-      case LADMIN_APPROVED_GROUP_CHANGE: // <event>newUserApprovedByLanguageAdministrator</event>
-        dialect = docCtx.getSourceDocument();
-
-        try {
-          util.placeNewUserInGroup(dialect,
-              (String) docCtx.getProperty(GROUP_NAME_ARG),
-              (String) docCtx.getProperty(USER_NAME_ARG));
-        } catch (Exception e) {
-          log.error(e);
-        }
-        break;
-
-      // this is in case of users joining Published dialects ie. Public
-      case SYSTEM_APPROVED_GROUP_CHANGE:
-        dialect = docCtx.getSourceDocument();
-
-        try {
-          util.systemPlaceNewUserInGroup(dialect,
-              (String) docCtx.getProperty(GROUP_NAME_ARG),
-              (String) docCtx.getProperty(USER_NAME_ARG),
-              dialect.getCoreSession());
-        } catch (Exception e) {
-          log.error(e);
-        }
-        break;
-
-      case "documentRemoved":
-        // TODO: use it to make sure user name is not left in the system when registration is
-        //  deleted on timeout
-        break;
-
-      // Event is fired after a user creates a password.
-      case INVITATION_VALIDATED:
-        args = docCtx.getArguments();
-
-        for (Object o : args) {
-          if (o == null) {
-            break;
-          }
-
-          DocumentModel registrationDoc = (DocumentModel) o;
-
-          if ("FVUserRegistration".equals(registrationDoc.getType())) {
-            completeUserRegistration(registrationDoc);
-          }
-        }
-        break;
-      default:
-        break;
+      if ("FVUserRegistration".equals(registrationDoc.getType())) {
+        completeUserRegistration(registrationDoc);
+      }
     }
   }
 
