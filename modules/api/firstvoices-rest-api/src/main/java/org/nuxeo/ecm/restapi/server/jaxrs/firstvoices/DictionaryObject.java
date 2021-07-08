768ebdb6cdf96cc166edfdce1735052216e024bd
Robert Johnstone <rob@plasticviking.com>
2021-07-08 20:04:49 +0800

    add categories to search, fix custom order

--- modules/api/firstvoices-rest-api/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/firstvoices/DictionaryObject.java
+++ modules/api/firstvoices-rest-api/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/firstvoices/DictionaryObject.java
@@ -18,7 +18,9 @@ import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.query.sql.NXQL;
 import org.nuxeo.ecm.webengine.model.WebObject;
 
 @WebObject(type = "dictionary")
@@ -28,10 +30,10 @@ public class DictionaryObject extends AbstractSearchlikeObject {
   @GET
   @Path("{dialect}")
   public Response getDictionary(
-      @PathParam(value = "dialect") String dialect,
-      @QueryParam(value = "q") String query,
+      @PathParam(value = "dialect") String dialect, @QueryParam(value = "q") String query,
       @QueryParam(value = "docType") @DefaultValue("WORDS_AND_PHRASES") String docType,
       @QueryParam(value = "alphabetCharacter") String alphabetCharacter,
+      @QueryParam(value = "category") String category,
       @QueryParam(value = "kidsOnly") @DefaultValue("false") boolean kidsOnly,
       @QueryParam(value = "gamesOnly") @DefaultValue("false") boolean gamesOnly,
       @QueryParam(value = "sortAscending") @DefaultValue("false") boolean sortAscending,
@@ -43,11 +45,18 @@ public class DictionaryObject extends AbstractSearchlikeObject {
       perPage = 100;
     }
     final int offset = (from - 1) * perPage;
+    DocumentModel categoryDocument = null;
 
     try {
       DocumentModel dialectDocument = getContext().getCoreSession().getDocument(new IdRef(dialect));
       if (!"FVDialect".equals(dialectDocument.getType())) {
         throw new IllegalArgumentException("Not a dialect");
+      }
+      if (category != null && category.length() > 0) {
+        categoryDocument = getContext().getCoreSession().getDocument(new IdRef(dialect));
+        if (!"FVCategory".equals(dialectDocument.getType())) {
+          throw new IllegalArgumentException("Not a category");
+        }
       }
     } catch (Exception e) {
       throw new IllegalArgumentException();
@@ -73,6 +82,10 @@ public class DictionaryObject extends AbstractSearchlikeObject {
     }
 
     BoolQueryBuilder combinedQuery = new BoolQueryBuilder().must(basicConstraints);
+
+    if (categoryDocument != null) {
+      basicConstraints = basicConstraints.must(QueryBuilders.matchQuery("category_ids", category));
+    }
 
     if (query != null) {
 
@@ -101,7 +114,21 @@ public class DictionaryObject extends AbstractSearchlikeObject {
     }
 
     if (alphabetCharacter != null && alphabetCharacter.length() > 0) {
-      PrefixQueryBuilder alphabetQuery = QueryBuilders.prefixQuery("dc:title", alphabetCharacter);
+      String substitutedCharacter = null;
+
+      DocumentModelList characterSearchResults = getContext().getCoreSession().query(
+          
+          "SELECT * " + "from FVCharacter where ecm:ancestorId = " + NXQL.escapeString(dialect)
+              + " and " + "dc:title = " + NXQL.escapeString(alphabetCharacter) + " and "
+              + "ecm:isVersion=0 and ecm:isTrashed=0 and ecm:isProxy=0");
+
+      if (characterSearchResults.size() == 1) {
+        DocumentModel fvchar = characterSearchResults.get(0);
+        substitutedCharacter = (String) fvchar.getPropertyValue("fvcharacter:alphabet_order");
+      }
+
+      PrefixQueryBuilder alphabetQuery = QueryBuilders.prefixQuery("dc:title",
+          substitutedCharacter != null ? substitutedCharacter : alphabetCharacter);
       combinedQuery.must(alphabetQuery);
     }
 
