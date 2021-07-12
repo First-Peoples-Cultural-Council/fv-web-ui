2c094b5c348cf13386faa5a98be6abe44d1a5bb4
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2021-07-12 16:32:01 -0700

    Adjust styling

--- frontend/app_v2/src/components/Category/CategoryPresentation.js
+++ frontend/app_v2/src/components/Category/CategoryPresentation.js
@@ -25,56 +25,19 @@ function CategoryPresentation({
   moreActions,
   sitename,
 }) {
-  const getConditionalClass = (category) => {
-    if (category.id === currentCategory.id || category.id === currentParentCategory.id) {
-      return 'border-l-4 border-tertiaryB bg-tertiaryB text-white'
-    }
-    return 'text-tertiaryB'
-  }
-
-  const getCurrentCategory = () => {
-    return (
-      <>
-        <Link
-          className="transition duration-500 ease-in-out md:my-2 md:mx-5 rounded-lg flex items-center cursor-pointer border-l-4 border-tertiaryB bg-tertiaryB text-white"
-          to={`/${sitename}/categories/${currentParentCategory.id}?docType=${docType}`}
-        >
-          {useCategoryIcon(currentParentCategory.title, 'inline-flex p-2 rounded-lg fill-current h-10 w-10')}
-          <div className="inline-flex text-lg font-medium">{currentParentCategory.title}</div>
-        </Link>
-        {currentParentCategory?.children?.length > 0
-          ? currentParentCategory?.children?.map((child) => {
-              return (
-                <Link
-                  key={child.id}
-                  className={`transition duration-500 ease-in-out md:my-2 md:mr-5 ml-10 rounded-lg flex items-center cursor-pointer ${getConditionalClass(
-                    child
-                  )}`}
-                  to={`/${sitename}/categories/${child.id}?docType=${docType}`}
-                >
-                  {useCategoryIcon(child.title, 'inline-flex p-2 rounded-lg fill-current h-10 w-10')}
-                  <div className="inline-flex text-lg font-medium">{child.title}</div>
-                </Link>
-              )
-            })
-          : null}
-      </>
-    )
-  }
-
   const getFilterListItems = () => {
     return categories.map((category) => {
       return (
         <li
           key={category.id}
           id={'SearchFilter' + category.id}
-          className="inline-block transition duration-500 ease-in-out md:block md:my-2 md:mx-5 flex-grow rounded-lg text-tertiaryB"
+          className="inline-block md:block transition duration-500 ease-in-out md:my-2 md:mx-5 flex-grow "
         >
           <Link
-            className={'transition duration-500 ease-in-out flex items-center  cursor-pointer '}
+            className="transition duration-500 ease-in-out flex items-center cursor-pointer rounded-lg text-tertiaryB"
             to={`/${sitename}/categories/${category.id}?docType=${docType}`}
           >
-            {useCategoryIcon(category.title, 'inline-flex p-2 rounded-lg fill-current h-10 w-10')}
+            {useCategoryIcon(category.title, 'inline-flex p-2 rounded-lg fill-current h-14 w-14')}
             <div className="inline-flex text-lg font-medium">{category.title}</div>
           </Link>
         </li>
@@ -83,11 +46,54 @@ function CategoryPresentation({
   }
   return (
     <>
-      <h2 className="hidden md:block text-2xl ml-8">Categories</h2>
       <div className="grid grid-cols-11 md:p-2">
-        <div className="col-span-11 md:col-span-2 mt-2  divide-y-4 divide-gray-300">
-          <div className="inline-flex align-center md:block m-2 md:m-0 h-56">{getCurrentCategory()}</div>
-          <ul className="inline-block md:block list-none m-2 md:m-0 md:space-y-4">{getFilterListItems()}</ul>
+        <div className="col-span-11 md:col-span-2 mt-2 md:mt-5">
+          <div className="inline-block md:block md:p-3">
+            <ul
+              className={`list-none m-2 md:space-y-4 ${
+                currentCategory?.id === currentParentCategory?.id ? 'border-l-4 border-tertiaryB' : ''
+              }`}
+            >
+              <li
+                key={currentParentCategory.id}
+                id={'SearchFilter' + currentParentCategory.id}
+                className="inline-block md:block transition duration-500 ease-in-out md:my-2 md:mx-5 flex-grow"
+              >
+                <Link
+                  className="transition duration-500 ease-in-out rounded-lg flex items-center cursor-pointer text-tertiaryB bg-gray-300"
+                  to={`/${sitename}/categories/${currentParentCategory.id}?docType=${docType}`}
+                >
+                  {useCategoryIcon(currentParentCategory.title, 'inline-flex p-2 rounded-lg fill-current h-14 w-14')}
+                  <div className="inline-flex text-lg font-medium">{currentParentCategory.title}</div>
+                </Link>
+              </li>
+              {currentParentCategory?.children?.length > 0
+                ? currentParentCategory?.children?.map((child) => {
+                    return (
+                      <li
+                        key={child.id}
+                        id={'SearchFilter' + child.id}
+                        className={`inline-block md:block transition duration-500 ease-in-out md:my-2 md:mx-5 flex-grow ${
+                          child.id === currentCategory.id ? 'border-l-4 border-tertiaryB' : ''
+                        }`}
+                      >
+                        <Link
+                          className={`transition duration-500 ease-in-out md:ml-10 rounded-lg flex items-center cursor-pointer text-tertiaryB ${
+                            child.id === currentCategory.id ? 'bg-gray-300' : ''
+                          }`}
+                          to={`/${sitename}/categories/${child.id}?docType=${docType}`}
+                        >
+                          <div className="p-2 h-10 inline-flex text-lg font-medium">{child.title}</div>
+                        </Link>
+                      </li>
+                    )
+                  })
+                : null}
+            </ul>
+          </div>
+          <div className="inline-block md:block md:p-3">
+            <ul className="list-none m-2 pt-5 md:space-y-4 border-t-2 border-gray-300">{getFilterListItems()}</ul>
+          </div>
         </div>
         <div className="min-h-220 col-span-11 md:col-span-9">
           <DictionaryListPresentation
